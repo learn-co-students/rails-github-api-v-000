69dd1579c30ce8226ff8bd735695b2beb487d673
jakebrady5 <jakebrady5@gmail.com>
2016-03-05 16:40:19 -0500

    just need to create

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,16 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get "https://api.github.com/user/repos", {
+      access_token: session[:token]
+    }
+    @body = JSON.parse(resp.body)
   end
 
   def create
-    
+    resp = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # resp = Faraday.post "https://api.github.com/user/repos", {name: params[:name], {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}}
+    # body = JSON.parse(resp.body)
+    binding.pry
+    redirect_to '/'
   end
 end
