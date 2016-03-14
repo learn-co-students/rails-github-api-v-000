a6e7f9b1e873182d7ab9f5f491843dd6f5a62575
Chen Yang <yangc5@mail.uc.edu>
2016-03-14 02:01:13 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.headers['Accept'] = 'application/json'
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params[:code]
+      req.params['redirect_uri'] = "http://yangc5-v-000-150060.nitrousapp.com:3000/auth"
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
+
   end
 end
