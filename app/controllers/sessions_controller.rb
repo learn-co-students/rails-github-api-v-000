54d2f6ab7bbe0abae9b5dd96a3d2e48803d1610f
dannyd4315 <dannyd4315@gmail.com>
2016-03-22 15:33:02 +1100

    Complete in browser, tests not passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+  
+  skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+      req.headers['Accept'] = 'application/json'
+      req.params['client_id'] = ENV['GITHUB_CLIENT']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['code'] = params[:code]
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
