e8e168939a71b20e7979cf5d37e3a26f5dee45ea
azheng249 <andy.zheng249@gmail.com>
2016-04-28 00:41:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user
+
+  helper_method :username
+
   private
 
     def authenticate_user
+      client_id = ENV['GITHUB_CLIENT']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")      
+      github_url = "https://github.com/login/oauth/authorize/?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=repo"
+      redirect_to github_url unless logged_in?
     end
 
     def logged_in?
+      !!session[:token]
+    end
+
+    def username
+      session[:username]
     end
 end
