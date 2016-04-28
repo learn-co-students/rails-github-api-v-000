641afe011affdb999e71e4decc30af08fb58b846
febbraiod <donedesign1@gmail.com>
2016-04-28 09:58:20 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,16 @@
 class RepositoriesController < ApplicationController
+  
+
   def index
+    @user_name = session[:username]
+
+    user_info = Faraday.get 'https://api.github.com/user/repos', {}, {'Authorization': "token #{session[:token]}"}
+    @user_repos = JSON.parse(user_info.body)
   end
 
   def create
+    resp = Faraday.post 'https://api.github.com/user/repos', {name: params[:name]}.to_json, {'Authorization': "token #{session[:token]}"}
+
+    redirect_to root_path
   end
 end
