ca7c8d1522c3c146d68633a1bd29d76f5edb16ff
Karuna <imkaruna@gmail.com>
2016-05-05 00:33:35 -0400

    Everything works but Webmock is throwing WebMock::NetConnectNotAllowedError:

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,21 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    #  raise resp.inspect
+    @repos = JSON.parse(resp.body)
+    render :index
+
   end
 
   def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+      req.body = {name: params[:name]}.to_json
+    end
+    redirect_to '/'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+    req.params['client_id'] = ENV['GIT_CLIENT_ID']
+    req.params['client_secret'] = ENV['GIT_CLIENT_SECRET']
+    req.params['redirect_uri'] = "http://localhost:3000/auth"
+    req.params['code'] = params[:code]
+    req.headers['Accept'] = 'application/json'
   end
+  body = JSON.parse(resp.body)
+  session[:token] = body["access_token"]
+  session[:username] = body["login"]
+  redirect_to root_path
+  end
+
+
+
 end
