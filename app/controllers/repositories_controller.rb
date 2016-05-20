4bc5fbb0dcd5f89983c3d60d18916e8ef0276839
fiveinfinity <jlachance1@gmail.com>
2016-05-19 19:45:16 -0500

    added Github API POST with Faraday

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,12 +1,12 @@
 class RepositoriesController < ApplicationController
   def index
     repo_response = Faraday.get("https://api.github.com/user/repos") do |req|
-      req.headers = { "Authorization": "token #{session[:token]}" }
+      req.headers = {Authorization: "token #{session[:token]}"}
     end
     @repos = JSON.parse(repo_response.body)
 
     user_response = Faraday.get("https://api.github.com/user") do |req|
-      req.headers = { "Authorization": "token #{session[:token]}" }
+      req.headers = {Authorization: "token #{session[:token]}"}
     end
     user_body = JSON.parse(user_response.body)
     @username = user_body["login"]
@@ -14,8 +14,8 @@ class RepositoriesController < ApplicationController
 
   def create
     response = Faraday.post("https://api.github.com/user/repos") do |req|
-      req.headers = { "Authorization": "token #{session[:token]}" }
-      req.body = { name: params[:name] }.to_json
+      req.headers = {Authorization: "token #{session[:token]}"}
+      req.body = {name: params[:name]}.to_json
     end
     redirect_to root_path
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,17 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.body = {
-        'client_id': ENV['CLIENT_ID'],
-        'client_secret': ENV['CLIENT_SECRET'],
-        'code': params[:code]
-      }
-      req.headers = { 'Accept': 'application/json' }
-    end
+    response = Faraday.post "https://github.com/login/oauth/access_token",
+      {
+        client_id: ENV["GITHUB_CLIENT"],
+        client_secret: ENV["GITHUB_SECRET"],
+        code: params[:code]
+      },
+        {'Accept' => 'application/json'}
 
-    body = JSON.parse(response.body)
-    session[:token] = body["access_token"]
-    redirect_to root_path
+    user_session = JSON.parse(response.body)
+    session[:token] = user_session["access_token"]
+
+    redirect_to '/'
   end
 end
