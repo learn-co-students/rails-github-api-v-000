fb627a67d06941b0d8258e729a6d8d7a874741f5
the-widget <brennenawana108ny@gmail.com>
2016-05-30 18:10:39 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,14 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers['authorization'] = "token #{session[:token]}"
+      req.headers['accept'] = 'application/json'
+    end
+    @repos = JSON.parse(resp.body)
   end
 
   def create
   end
 end
+
+
