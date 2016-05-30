0cfef9a28ac9bbf91dca56c5d2445580c520b97d
the-widget <brennenawana108ny@gmail.com>
2016-05-30 19:19:05 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
-    client_id = ENV['GITHUB_CLIENT_ID']
+    client_id = ENV['GITHUB_CLIENT']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
-    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo&redirect_uri=#{redirect_uri}"
     redirect_to github_url unless logged_in?
   end
     
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
   def create
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_id'] = ENV['GITHUB_CLIENT']
       req.params['client_secret'] = ENV['GITHUB_SECRET']
       req.params['redirect_uri'] = "http://localhost:3000/auth"
       req.params['code'] = params[:code]
