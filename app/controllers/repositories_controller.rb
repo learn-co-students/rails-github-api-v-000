bb19b57ced7e92034e0330dd9131f448ae0a6e51
pacosta613 <pedro.acosta613@gmail.com>
2016-06-07 14:06:18 -0400

    Test not passing but getting results

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,20 @@
 class RepositoriesController < ApplicationController
+
   def index
+    @resp = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers["Accept"] = "application/json"
+    end
+    @username = JSON.parse(@resp.body).first["owner"]["login"]
+    @results = JSON.parse(@resp.body)
   end
 
   def create
+    @resp = Faraday.post "https://api.github.com/user/repos" do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.body = {name: "#{params[:name]}"}.to_json
+    end
+    redirect_to '/'
   end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,22 @@ class SessionsController < ApplicationController
   def create
     @resp = Faraday.post "https://github.com/login/oauth/access_token" do |req|
       req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params[:code]
+      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.headers['Accept'] = 'application/json'
     end
+    body = JSON.parse(@resp.body)
+    session[:token] = body["access_token"]
+    
+    @get_user = Faraday.get "https://api.github.com/user" do |req|
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    user_body = JSON.parse(@get_user.body)
+    session[:username] = user_body["login"]
+    
+    redirect_to root_path
   end
 
 end
