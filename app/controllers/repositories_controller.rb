a7042029518ace88122c54f7d9ddc090f68b3f4f
Tyler Smith <oats000@hotmail.com>
2016-06-07 03:14:01 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,27 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get("https://api.github.com/user") do |req|
+      req.headers['Authorization'] =  "token #{session[:token]}"
+    end
+    # binding.pry
+    @user = JSON.parse(resp.body)
+
+    resp2 = Faraday.get("https://api.github.com/users/#{@user['login']}/repos")
+    @repos = Kaminari.paginate_array(JSON.parse(resp2.body)).page(params[:page])
   end
 
   def create
+
+    # resp2 = Faraday.get("https://api.github.com/user") do |req|
+    #   req.headers['Authorization'] =  "token #{session[:token]}"
+    # end
+    # # binding.pry
+    # @user = JSON.parse(resp2.body)
+
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.body = "{'name' => #{params['name']}}"
+      req.headers['Authorization'] =  "token #{session[:token]}"
+    end
+    redirect_to '/'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = 'a4d3cdd8d8ff49fa54c7'
+      req.params['client_secret'] = 'c7e0bf60f7bd3eb4d351078aaf61fffb64c26d24'
+      # req.params['grant_type'] = 'authorization_code'
+      req.params['redirect_uri'] = "http://159.203.117.55:3098/auth"
+      req.params['code'] = params[:code]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
