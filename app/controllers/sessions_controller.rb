710bf3cc7adc72a233cae2b2fefbb49419cef6cb
LKWLaLa <lkwlala@gmail.com>
2016-06-15 15:30:00 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
 
    skip_before_action :authenticate_user, only: :create
-=begin
+
+
   def create
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT_ID"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
+
     session[:token] = access_hash["access_token"]
 
     user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
@@ -12,11 +14,13 @@ class SessionsController < ApplicationController
     session[:username] = user_json["login"]
 
     redirect_to '/'
+
   end
-=end
 
 
 
+=begin
+First try!
   def create
     resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
       req.body = "{ 
@@ -25,6 +29,7 @@ class SessionsController < ApplicationController
       'code': #{params[:code]} 
         }"
     end
+    raise resp.inspect
  
     body = JSON.parse(resp.body)
     session[:token] = body["access_token"]
@@ -32,7 +37,24 @@ class SessionsController < ApplicationController
    
     redirect_to "/"
   end
+=end
 
+=begin
+#Chad's try!
+  def create
+    resp = Faraday.post do |req|
+      req.url 'https://github.com/login/oauth/access_token'
+      req.headers['Content-Type'] = 'application/json'
+      req.body = "{
+      'client_id': #{ENV['GITHUB_CLIENT_ID']}, 'client_secret': #{ENV['GITHUB_SECRET']}, 'code': #{params[:code]}
+        }" 
+    end
 
+    raise resp.inspect
 
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to "/"
+  end
+=end
 end
