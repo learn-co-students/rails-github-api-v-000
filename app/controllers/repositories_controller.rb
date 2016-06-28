1e89c33debff302e1c358deb32ea18805ed0e269
gormanjp <jpgorman8@gmail.com>
2016-06-27 23:50:30 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,23 @@
 class RepositoriesController < ApplicationController
   def index
+  	resp = Faraday.get("https://api.github.com/user/repos") do |req|
+  	req.headers['Authorization'] = "token #{session[:token]}"
+  	req.headers['Accept'] = 'application/json'
+  end
+
+    user_resp = Faraday.get("https://api.github.com/user") do |req|
+  	req.headers['Authorization'] = "token #{session[:token]}"
+  	req.headers['Accept'] = 'application/json'
+  end
+  	@user = JSON.parse(user_resp.body)
+  	@repos = JSON.parse(resp.body)
   end
 
   def create
+  	response = Faraday.post("https://api.github.com/user/repos") do |req|
+  		req.headers['Authorization'] = "token #{session[:token]}"
+  		req.body = {name: params['name']}.to_json
+  	end
+  	redirect_to root_path
   end
 end
