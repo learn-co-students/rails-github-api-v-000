eb22d1e8a9ab71e6285ba8d5f5e34febd5de3fa2
McN33ly <rmcneely23@gmail.com>
2016-07-12 11:36:13 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,15 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: [:create]
   def create
     # binding.pry
-    resp = Faraday.post('https://github.com/login/oauth/authorize/access_token') do |options|
+    # f = Faraday.new(url: 'https://github.com/login/oauth/authorize/access_token' )
+    @resp = Faraday.post('https://github.com/login/oauth/authorize/access_token') do |options|
       options.params['client_id'] = ENV['GITHUB_KEY']
       options.params['client_secret'] = ENV['GITHUB_SECRET']
-      options.params['redirect_uri'] = 'http://localhost:3000/auth/github'
+      options.params['redirect_uri'] = 'http://localhost:3000/auth'
       options.params['code'] = params[:code]
       options.headers['Accept'] = 'application/json'
+      binding.pry
     end
-    binding.pry
-    response = JSON.parse(resp.body)
+    # response = JSON.parse(resp.body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/auth/github' => 'sessions#create'
+  get '/auth' => 'sessions#create'
   post '/repositories/create' => 'repositories#create'
   root 'repositories#index'
 
