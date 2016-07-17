5dfd1e850e6223592fc82991ec11632b8e8ba984
corya0687 <corya0687@gmail.com>
2016-07-17 17:41:08 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
 
     def current_user
       session[:username]= JSON.parse(Faraday.get("https://api.github.com/user") do |req|
-          req.params['oauth_token'] = session[:token]
+          req.headers['Authorization'] = "token #{session[:token]}"
         end.body)["login"]
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,19 @@ class SessionsController < ApplicationController
 
   def create
     response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.body= {
-        "client_id"=> ENV['GITHUB_CLIENT'],
-        "client_secret"=> ENV['GITHUB_SECRET'], "code"=> "#{params[:code]}"
+      req.body = {
+        'client_id'=> ENV['GITHUB_CLIENT'],
+        'client_secret'=> ENV['GITHUB_SECRET'],
+        'code'=> "#{params[:code]}"
       }
-      #req.headers['Accept']= "application/json"
+      req.headers = { 'Accept'=> 'application/json'}
     end
 
-    binding.pry
+
     token = JSON.parse(response.body)["access_token"]
     session[:token] = token
     @username = current_user
     binding.pry
-    redirect_to root_path
+    render "repositories/index"
   end
 end
