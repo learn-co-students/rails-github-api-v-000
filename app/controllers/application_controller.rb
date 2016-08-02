bed9ba28d09f3a3f47491ff958d800316c8f13e9
Smulligan85 <sean.mulligan85@gmail.com>
2016-08-02 09:26:44 -0700

    Final GH Omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user
       client_id = Rails.application.secrets.github_api_client_id
-      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
+      redirect_uri = CGI.escape('http://localhost:3000/auth')
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo&redirect_uri=#{redirect_uri}"
       redirect_to github_url unless logged_in?
     end
 
