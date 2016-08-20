e2059ab193b2cbdbdb87975441e109bfe4ba964b
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-20 14:51:59 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,33 @@
 class RepositoriesController < ApplicationController
   def index
+    binding.pry
+    resp = Faraday.post("https://api.github.com/oauth/access_token") do |req|
+      # need to show current user's repositories based on oauth token's scope
+      req.params['client_id']     = nil
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['code']          = '20'
+      # req.params['oauth_token']   = session[:token]
+      # hoping to grab params from form in index view below
+      # req.params['repo']          = params[:name]
+      # req.params['v']           = '20160201'
+      req.headers                 = {'Accept' => 'application/json'} 
+    end
+    @repositories
   end
 
   def create
+    resp = Faraday.get("https://api.github.com/users") do |req|
+      # need to show current user's repositories based on oauth token's scope
+
+      req.params['oauth_token'] = session[:token]
+      # hoping to grab params from form in index view below
+      req.params['repo']        = params[:name]
+      req.params['v']           = '20160201'
+    end
+    binding.pry
+
+    @repositories = JSON.parse(resp.body)#["response"]["repos"]
+    
+    redirect_to root_path
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post("https://GITHUB.com/login/oauth/access_token") do |req|
+      req.headers                 = {'Accept' => 'application/json'} 
+      req.params['client_id']     = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['state']         = 'authorization_code'
+      # req.params['code']          = params[:code]
+      req.params['redirect_uri']  = "http://localhost:3000/auth"
+    end
+    binding.pry
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
