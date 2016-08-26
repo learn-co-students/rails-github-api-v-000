49b7f96ef010cdad8c5afa63aa7bc897e8fa008d
michaelsimon <michael.simon@gmail.com>
2016-08-26 17:59:33 -0400

    build github auth and view current users repositories

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,13 @@
 class RepositoriesController < ApplicationController
   def index
+    repos = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.params["access_token"] = session[:token]
+  end
+  user = Faraday.get("https://api.github.com/user") do |req|
+    req.params["access_token"] = session[:token]
+  end
+  @user = JSON.parse(user.body)["login"]
+  @repos = JSON.parse(repos.body)
   end
 
   def create
