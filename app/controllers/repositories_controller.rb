694e29c923ec8d8e06d75b1b1289b44a48a52881
michaelsimon <michael.simon@gmail.com>
2016-08-26 21:51:27 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,15 +1,19 @@
 class RepositoriesController < ApplicationController
   def index
     repos = Faraday.get("https://api.github.com/user/repos") do |req|
-      req.params["access_token"] = session[:token]
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/jsonN'
   end
-  user = Faraday.get("https://api.github.com/user") do |req|
-    req.params["access_token"] = session[:token]
-  end
-  @user = JSON.parse(user.body)["login"]
   @repos = JSON.parse(repos.body)
   end
 
   def create
+    repo_name = params[:name]
+    create = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+      req.body = {name: repo_name}.to_json
+    end
+    redirect_to '/'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,26 @@
 class SessionsController < ApplicationController
-    skip_before_action :authenticate_user, only: :create
+  skip_before_action :authenticate_user, only: :create
 
   def create
-    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.headers["Accept"] ='application/json'
-      req.params["client_id"] = ENV['GITHUB_CLIENT']
-      req.params["client_secret"] = ENV['GITHUB_SECRET']
-      req.params["code"] = params[:code]
-      req.params["state"] = ENV['STATE']
-      req.params["redirect_uri"] = "http://localhost:3000/auth"
+    #what i originally had / webmock doesn't like block
+    # resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+    #   req.params['client_id'] = ENV['GITHUB_CLIENT']
+    #   req.params['client_secret'] = ENV['GITHUB_SECRET']
+    #   req.params['code'] = params[:code]
+    #   req.headers['Accept'] = 'application/json'
+    # end
+     resp = Faraday.post 'https://github.com/login/oauth/access_token', {client_id: ENV['GITHUB_CLIENT'], client_secret: ENV['GITHUB_SECRET'], code: params[:code]}, {Accept: 'application/json'}
+
+    session[:token] = JSON.parse(resp.body)["access_token"]
+
+    user = Faraday.get('https://api.github.com/user') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
     end
-    @resp = JSON.parse(resp.body)
-    session[:token] = @resp["access_token"]
-    redirect_to root_path
+
+    session[:username] = JSON.parse(user.body)["login"]
+    redirect_to '/'
+
   end
+
 end
