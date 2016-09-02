75edbccbd9cf36bd117883c2f1ff125bf0152a95
Jason <jasonkwong11@gmail.com>
2016-09-02 17:20:24 -0400

    done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,25 @@
 class RepositoriesController < ApplicationController
   def index
+    user_resp = Faraday.get("https://api.github.com/user") do |req|
+      req.headers['authorization'] = "token #{session[:token]}"
+      req.headers['accept'] = 'application/json'
+    end
+    @username = JSON.parse(user_resp.body)["login"]
+
+    repo_resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers["Accept"] = "application/jsonN"
+    end
+    @repos = JSON.parse(repo_resp.body)
   end
 
   def create
+    new_repo_name = params[:name]
+    new_repo = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+      req.body = {name: new_repo_name}.to_json
+    end
+    redirect_to root_path
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
+require 'dotenv'
 require File.expand_path('../application', __FILE__)
-
+Dotenv.load
 # Initialize the Rails application.
 Rails.application.initialize!
