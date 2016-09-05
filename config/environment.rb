b350f64b22456f9396b1102cea5d38babd468824
Saturn226 <serenehopes@gmail.com>
2016-09-05 01:13:22 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
+require 'dotenv'
 require File.expand_path('../application', __FILE__)
+Dotenv.load
 
 # Initialize the Rails application.
 Rails.application.initialize!
