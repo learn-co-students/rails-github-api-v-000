d5e7d099ac0f95807331938f8e506a7e55722360
gabe <gj+code@mail.co.de>
2016-10-04 02:39:59 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ group :development, :test do
   gem 'spring'
 end
 
+gem 'kaminari'
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,18 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get 'https://api.github.com/user/repos', {}, { 'Authorization': "token #{session[:token]}", 'Accept': 'application/json' }
+
+    # raise JSON.parse(resp.body).inspect
+
+    @repos = Kaminari.paginate_array(JSON.parse(resp.body)).page(params[:page])
   end
 
   def create
+    resp = Faraday.post 'https://api.github.com/user/repos' do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+      req.body = { 'name': params[:name] }.to_json
+    end
+    redirect_to root_path
   end
 end
