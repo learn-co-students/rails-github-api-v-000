462cd706d88359bee614002373a9d614d20d5112
Kailee <kaileeagray@gmail.com>
2016-10-26 21:27:59 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
 
-private
+  private
 
-  def authenticate_user
-    client_id = ENV['GITHUB_CLIENT_ID']
-    redirect_to "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo" if !logged_in?
-  end
+    def authenticate_user
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+    end
 
-  def logged_in?
-    !!session[:token]
-  end
+    def logged_in?
+      !!session[:token]
+    end
 end
