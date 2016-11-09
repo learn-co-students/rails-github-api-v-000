4b36810acf7f4c0fa3a42a0e29fe0f77f52fc719
jbok4 <jbok4@aol.com>
2016-11-09 13:58:24 -0500

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+protect_from_forgery with: :exception
   before_action :authenticate_user
-
 
   private
 
     def authenticate_user
-      # client_id = ENV['FOURSQUARE_CLIENT_ID']
-      # redirect_uri = CGI.escape("http://localhost:3000/auth")
-      # foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
-      # redirect_to foursquare_url unless logged_in?
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
     end
 
     def logged_in?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
 class SessionsController < ApplicationController
-  def create
-    # resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
-    #   req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
-    #   req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
-    #   req.params['grant_type'] = 'authorization_code'
-    #   req.params['redirect_uri'] = "http://localhost:3000/auth"
-    #   req.params['code'] = params[:code]
-    # end
+  skip_before_action :authenticate_user, only: :create
 
-    # body = JSON.parse(resp.body)
-    # session[:token] = body["access_token"]
-    # redirect_to root_path
+  def create
+    response = Faraday.post "https://github.com/login/oauth/access_token", 
+    {
+      client_id: ENV["GITHUB_CLIENT"], 
+      client_secret: ENV["GITHUB_SECRET"],
+      code: params[:code]}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    session[:token] = access_hash["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, 
+    {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+
+    redirect_to '/'
   end
+
 end
