830e2744ccddebfcc7931681ab0cefd9d5d8f5eb
steveafrost <ffrostt@gmail.com>
2016-11-13 22:49:32 -0500

    used Faraday gem to access GitHub oAuth to list & create new repos

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,20 @@
 class RepositoriesController < ApplicationController
   def index
+    repo_resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @repos = JSON.parse(repo_resp.body)
   end
 
   def create
+    Faraday.post('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+      req.body = {name: params[:name]}.to_json
+    end
+
+    redirect_to root_path
   end
 end
