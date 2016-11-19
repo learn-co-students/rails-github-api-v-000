a49a3aeba8678a28fdba97ebddf5af1f9b1cb28d
tpstar <hanlee12@gmail.com>
2016-11-19 14:33:08 -0600

    done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,27 +1,18 @@
 class RepositoriesController < ApplicationController
   def index
+    repo_data = Faraday.get("https://api.github.com/user/repos",
+                            {},
+                            {'Authorization' => "token #{session[:token]}",
+                             'Accept' => 'application/json'})
+    @repos = JSON.parse(repo_data.body)
+    # raise @repos.inspect
   end
 
   def create
-    # resp = Faraday.post("https://api.foursquare.com/v2/tips/add") do |req|
-    #   req.params['oauth_token'] = session[:token]
-    #   req.params['v'] = '20161116'
-    #   req.params['venueId'] = params[:venue_id]
-    #   req.params['text'] = params[:tip]
-    # end
-    # redirect_to tips_path
 
-    # resp = Faraday.post("https://github.com/login/oauth/access_token",
-    #                     {client_id: ENV["GITHUB_CLIENT"],
-    #                      client_secret: ENV["GITHUB_SECRET"],
-    #                      code: params[:code]},
-    #                     {'Accept' => 'application/json'})
-    # session[:token] = JSON.parse(resp.body)["access_token"]
-
-    resp = Faraday.post("https://api.github.com/user/repos",
-                        {name: params[:name]},
-                        {'Authorization' => "token #{session[:token]}"})
-    reposit_name = JSON.parse(resp.body)["name"]
+    response = Faraday.post("https://api.github.com/user/repos",
+                            {name: params[:name]}.to_json,
+                            {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'})
     redirect_to root_path
   end
 end
