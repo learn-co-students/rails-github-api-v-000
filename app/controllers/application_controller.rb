dd2b06337e815a1070b17940a2c4ca2b2dca96ab
karuna24s <karuna24s@gmail.com>
2016-11-22 22:22:20 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,7 @@ class ApplicationController < ActionController::Base
     def authenticate_user
       client_id = ENV['GITHUB_CLIENT_ID']
       redirect_uri = CGI.escape("http://localhost:3000/auth")
-      scope = 'public_repo'
-      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=#{scope}"
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
       redirect_to github_url unless logged_in?
     end
 
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 Dotenv::Railtie.load
-GITHUB = ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
+FOURSQUARE = ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
 
 module GithubDemo
   class Application < Rails::Application
