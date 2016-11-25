1d4e59fd1b21e1d7573d513bc8dc274998878198
Caleb Faught <faughtcj@gmail.com>
2016-11-24 19:06:06 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/auth' => 'sessions#create'
   post '/repositories/create' => 'repositories#create'
   root 'repositories#index'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
