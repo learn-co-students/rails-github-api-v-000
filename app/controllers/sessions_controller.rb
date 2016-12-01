78da3f30740b19b70e563d5a84ddeb1842da232f
tylertaylor <vikingtyty@gmail.com>
2016-12-01 09:30:19 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.headers['Accept'] = 'application/json'
+      req.body = {
+        client_id: ENV["GITHUB_CLIENT"], 
+        client_secret: ENV["GITHUB_SECRET"],
+        code: params[:code]
+      }
+    end
+
+    # this was the solution even though the style above was used in the readme...
+    # why would this work?
+    # resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+    
+    redirect_to '/'
   end
 end
