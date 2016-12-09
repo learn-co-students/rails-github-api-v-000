301770a2b44862a25d33a770dd69a1fcab1da17c
bedle88 <robey.mc@gmail.com>
2016-12-09 16:32:39 -0500

    done despite trouble with specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     def authenticate_user
       client_id = ENV['CLIENT_ID']
       redirect_uri = CGI.escape('http://localhost:3000/auth')
-      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=public_repo"
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
       redirect_to github_url unless logged_in?
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
-      req.params['client_id'] = ENV['GITHUB_CLIENT']
-      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['client_id'] = ENV['CLIENT_ID']
+      req.params['client_secret'] = ENV['CLIENT_SECRET']
       req.params['code'] = params[:code]
     end
     body = Rack::Utils.parse_nested_query(resp.body)
