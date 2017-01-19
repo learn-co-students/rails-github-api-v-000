ae74e96fafbb09603ff65045ccb5a1800a91f4e3
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-01-18 18:07:29 -0800

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,20 @@
 class RepositoriesController < ApplicationController
   def index
+  	@username = current_user
+  	resp = Faraday.get("https://api.github.com/users/#{@username}/repos") do |req|
+  		req.headers['Authorization'] = "token #{session[:token]}" 
+  	end
+  	@repos = JSON.parse(resp.body)
+
   end
 
   def create
+  	resp = Faraday.post("https://api.github.com/user/repos") do |req|
+  		req.body = { "name" => params[:name] }.to_json
+  		req.headers['Accept'] = 'application/json'
+  		req.headers['Authorization'] = "token #{session[:token]}" 
+  	end
+  	puts resp.body
+  	redirect_to root_path
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def current_user
+		resp = Faraday.get("https://api.github.com/user") do |req|
+  			req.headers["Authorization"] = "token #{session[:token]}"
+  		end
+  		body = JSON.parse(resp.body)
+  	    return body["login"]
+	end
 end
