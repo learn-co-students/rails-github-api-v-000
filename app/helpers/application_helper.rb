a05609a9306b6cbe433ce64bdc5cfc0212a99eac
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-01-18 21:41:59 -0800

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 	def current_user
 		resp = Faraday.get("https://api.github.com/user") do |req|
   			req.headers["Authorization"] = "token #{session[:token]}"
+  			req.headers['Accept'] = 'application/json'
   		end
   		body = JSON.parse(resp.body)
   	    return body["login"]
