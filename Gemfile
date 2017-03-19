0adf4c9328681826a69440c267071eb5df352e8b
Adam Friedl <ajfriedl@gmail.com>
2017-03-19 17:25:22 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+# Fix console and server with readline
+gem 'rb-readline'
 
 # Use Pry for testing
 gem 'pry'
@@ -57,4 +59,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,31 @@
+# class SessionsController < ApplicationController
+#   skip_before_action :authenticate_user, only: :create
+#
+#   def create
+#     response = Faraday.post('https://github.com/login/oauth/access_token'), {client_id: ENV['client_id'], client_secret: ENV['client_secret'], code: params[:code]}, {'Accept'=> 'application/json'}
+#
+#     binding.pry
+#     session[:token] = params['access_token']
+#     user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+#     user_json = JSON.parse(user_response.body)
+#     session[:username] = user_json["login"]
+#
+#
+#     redirect_to root_path
+#   end
+# end
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["client_id"], client_secret: ENV["client_secret"],code: params[:code]}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    session[:token] = access_hash["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+
+    redirect_to '/'
   end
 end
