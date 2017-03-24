e17c2a120a0eecb6843f7a9c4f637913f25a4ff7
smithwebtek <brad@smithwebtek.com>
2017-03-23 21:14:29 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,23 +1,10 @@
 class RepositoriesController < ApplicationController
   def index
-
-    # List your repositories (accessible to the authenticated user).
-      # GET /user/repos
-      # shows my repos within the 'learn-co-students' org
-  response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    
-
-# List user repositories(public repositories for the specified user).
-# shows all of my public repos, including any that I just created using the form in this lab
-# fails the tests because of  WebMock::NetConnectNotAllowedError
-# uses this url:                          GET /users/:username/repos
-# response = Faraday.get "https://api.github.com/users/#{session[:username]}/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     @repos = JSON.parse(response.body)
   end
 
   def create
-    # response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}  
     redirect_to '/'
   end
