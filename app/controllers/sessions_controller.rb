ce97123cfb464b9c9ca1427d159d7b15787a6051
github username <jilustrisimo@users.noreply.github.com>
2017-04-08 14:24:41 -0400

    able to get access_token and pass JSON body

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    conn = Faraday.new(url: 'https://github.com/login/oauth/access_token')
+    resp = conn.post do |req|
+      req.headers['Accept'] = 'application/json'
+      req.body = { client_id: ENV['GITHUB_CLIENT_ID'],
+                   client_secret: ENV['GITHUB_SECRET'],
+                   code: params[:code] }
+    end
+    body = JSON.parse(resp.body)
+    binding.pry
+    session[:token] = body['access_token']
+    redirect_to root_path
   end
 end
