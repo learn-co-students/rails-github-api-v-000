91fbe9f7e585ffbad19d76538c36d31710b1815c
github username <jilustrisimo@users.noreply.github.com>
2017-04-08 14:53:31 -0400

    built sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,15 @@ class SessionsController < ApplicationController
                    code: params[:code] }
     end
     body = JSON.parse(resp.body)
-    binding.pry
     session[:token] = body['access_token']
+
+    conn2 = Faraday.new(url: 'https://api.github.com/user')
+    resp2 = conn2.get do |req|
+      req.headers = { Accept: 'application/json',
+                      Authorization: "token #{session[:token]}" }
+    end
+    user = JSON.parse(resp2.body)
+    session[:username] = user['login']
     redirect_to root_path
   end
 end
