233992cf8a7b9275443382a76223df0f0de93623
Scott Savage <snsavage@gmail.com>
2017-04-13 20:04:35 -0400

    Pass all lab specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user
+
   private
 
     def authenticate_user
+      client_id = ENV['GITHUB_CLIENT']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      query = "client_id=#{client_id}&redirect_url=#{redirect_uri}"
+      github_url = "https://github.com/login/oauth/authorize?#{query}"
+
+      redirect_to github_url unless logged_in?
     end
 
     def logged_in?
+      !!session[:token]
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    url = 'https://github.com/login/oauth/access_token'
+    resp = Faraday.post(url) do |req|
+      req.headers['Accept'] = 'application/json'
+
+      req.body = {
+        'client_id':     ENV['GITHUB_CLIENT'],
+        'client_secret': ENV['GITHUB_SECRET'],
+        'code':          params['code']
+      }
+    end
+
+    body = JSON.parse(resp.body)
+
+    session[:token] = body['access_token']
+    redirect_to root_path
   end
 end
