f4ae7c0ebdc35ae862be3cc803dad00fbc117b77
github username <github email address>
2017-05-09 16:50:59 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,16 @@
 class RepositoriesController < ApplicationController
   def index
+    response = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.headers = {
+        "Authorization" => "token #{session[:token]}",
+        "Accept" => 'application/json'
+      }
+    end
+    @repos = JSON.parse(response.body)
   end
 
-  def create
+  def create #comes from index form
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to '/'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
-  def create
+  skip_before_action :authenticate_user, only: :create
+
+  def create #comes from '/auth'
+      response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+        req.params['client_id'] = ENV["client_id"]
+        req.params['client_secret'] = ENV["client_secret"]
+        req.params["code"] = params["code"]
+      end 
+      hash = JSON.parse(response.body)
+      session[:token] = hash["access_token"]
+      redirect_to root_path
   end
 end
+
+
+# Parameters
+# Name	Type	Description
+# client_id	string	Required. The client ID you received from GitHub when you registered.
+# client_secret	string	Required. The client secret you received from GitHub when you registered.
+# code	string	Required. The code you received as a response to Step 1.
+# redirect_uri	string	The URL in your application where users will be sent after authorization. See details below about redirect urls.
+# state	string	The unguessable random string you optionally provided in Step 1.
