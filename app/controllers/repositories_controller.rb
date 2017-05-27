ab6cb82d20e0b1bbc68aefd6765150affaa7ea25
Michael Ries <michael.ries@gmail.com>
2017-05-27 13:21:27 -0500

    save all

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,32 @@
 class RepositoriesController < ApplicationController
   def index
+    @repositories = getRepositories
   end
 
   def create
+    name = params[:name]
+    createRepository(name)
+    redirect_to root_path
+  end
+
+  def getRepositories
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+        req.params['access_token'] = session[:token]
+        req.headers = {Accept: 'application/json'}
+    end
+    
+    repos = JSON.parse(resp.body)
+  end
+
+  def createRepository(name)
+    body = ''
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+        req.params['access_token'] = session[:token]
+        req.body = '{ "name": "' + name + '"}'
+        body = req.body
+    end
+
+    response = JSON.parse(resp.body)
+    byebug
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,33 @@
 class SessionsController < ApplicationController
-  def create
-  end
+    skip_before_action :authenticate_user
+
+    def create
+        resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+            req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+            req.params['client_secret'] = ENV['GITHUB_SECRET']
+            req.params['grant_type'] = 'authorization_code'
+            req.params['redirect_uri'] = "http://localhost:3000/auth"
+            req.params['code'] = params[:code]
+            req.params['scope'] = 'repo'
+            req.headers = {Accept: 'application/json'}
+        end
+        
+        body = JSON.parse(resp.body)
+        session[:token] = body["access_token"]
+
+        getUser
+
+        redirect_to root_path
+    end
+
+    private
+    def getUser
+        resp = Faraday.get("https://api.github.com/user") do |req|
+            req.params['access_token'] = session[:token]
+            req.headers = {Accept: 'application/json'}
+        end
+        
+        body = JSON.parse(resp.body)
+        session[:username] = body['login']
+    end
 end
