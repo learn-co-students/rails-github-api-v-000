32b0704c7ae04a72427f1fd7f18af06f7994843c
kkterai <kkterai@gmail.com>
2017-06-02 11:46:27 -0700

    updated repo controller

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,6 @@
 class RepositoriesController < ApplicationController
   def index
+    puts session
     resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
 
    @repos = JSON.parse(resp.body)
