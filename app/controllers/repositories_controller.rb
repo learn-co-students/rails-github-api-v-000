f8b43c332fbed71c3b0e32c4381e1d0a38894bc5
kkterai <kkterai@gmail.com>
2017-06-03 09:54:37 -0700

    resolve difference

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,5 @@
 class RepositoriesController < ApplicationController
   def index
-    puts session
     resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
 
    @repos = JSON.parse(resp.body)
