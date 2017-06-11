bba739df377565d9b0c6ba54b4a31b1793512743
Seth <itz.saga@gmail.com>
2017-06-11 17:02:30 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     # byebug
-    resp = Faraday.post 'https://github.com/login/oauth/access_token'do |req|
+    resp = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
       req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
       req.params['client_secret'] = ENV['GITHUB_SECRET']
       req.params['code'] = params[:code]
