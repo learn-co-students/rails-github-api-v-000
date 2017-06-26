8cb28de9a25cf3f3e16b34d95f46b24d376b75fc
WilliamMena <williammena1991@gmail.com>
2017-06-25 21:29:11 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user
   private
 
     def authenticate_user
+      client_id = ENV['GITHUB_CLIENT']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      github_url = "http://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
+      get_user_info
+      redirect_to github_url unless logged_in?
     end
 
     def logged_in?
+      !!session[:token]
+    end
+
+    def get_user_info
+      resp = Faraday.get('https://api.github.com/user') do |req|
+        req.headers['Accept'] = 'application/json'
+        req.params['access_token'] = session[:token]
+      end
+      body = JSON.parse(resp.body)
+      @username = body['login']
     end
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,31 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers['Accept'] = 'application/json'
+      req.params[:access_token] = session[:token]
+    end
+    body = JSON.parse(resp.body)
+    @repos = body
   end
 
   def create
+    resp = Faraday.post('https://api.github.com/user/repos') do |req|
+      # req.headers['Content-Type'] = 'application/json'
+      req.headers = {
+        'Content-Type' => 'application/json',
+        'Authorization' => "token #{session[:token]}"
+       }
+      # req.headers['token'] = session[:token]
+      req.params['name'] = params[:name]
+      redirect_to root_path
+    end
+
+    # Faraday.get('https://api.github.com/repos/williammena/aaabbb') do |req|
+    #   req.headers['Accept'] = 'application/json'
+    #   req.params[:access_token] = session[:token]
+    # end
+
+
+    binding.pry
   end
 end
