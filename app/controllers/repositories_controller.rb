41a8470de1b9d8f0fd24ae7e6ed6f0387f3ec7a3
Menachem Hornbacher <me@mendel.tech>
2017-07-07 21:22:40 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,14 @@
 class RepositoriesController < ApplicationController
   def index
+    if session[:token] == "1"
+      @repositories = ["Repo 1", "Repo 2", "Repo 3"]
+    else
+      @repositories = ["Repo 1", "Repo 2", "Repo 3"]
+    end
   end
 
   def create
+    Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to root_path
   end
 end
