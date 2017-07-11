5d93a91caf615c230d210e7d56beeb0c303c5104
Hima Chitalia <hima_chhag@yahoo.com>
2017-07-11 13:39:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
+      #Users are redirected to request their GitHub identity
       client_id = ENV['GITHUB_CLIENT']
       github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
       redirect_to github_url unless logged_in?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,19 @@ class SessionsController < ApplicationController
  skip_before_action :authenticate_user
 
   def create
+    #If the user accepts your request, GitHub redirects back to your site with a temporary code in a code parameter
+    #Exchange this code for an access token: with the post request
 
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     session[:token] = access_hash["access_token"]
 
+    #Use the access token to access the API. The access token allows you to make requests to the API on a behalf of a user.
+    #You can pass the token in the query params as shown above, but a cleaner approach is to include it in the Authorization header. Authorization: token OAUTH-TOKEN
+
     user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     user_json = JSON.parse(user_response.body)
+    @user = user_json["login"]
     session[:username] = user_json["login"]
 
     redirect_to '/'
