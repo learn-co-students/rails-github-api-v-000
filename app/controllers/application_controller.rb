ef7023130a62b7e2bd0b2d015909603bc1553ee1
Howard DeVennish <howarddevennish@gmail.com>
2017-08-07 16:57:46 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,12 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def authenticate_user
-      client_id = ENV['CLIENT_ID']
-      client_secret = ENV['CLIENT_SECRET']
-      # set redirect url and go to github authentication url unless user is logged in
-      github_url = "" #set github url
-      redirect_to github_url unless logged_in?
-    end
+  def authenticate_user
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['CLIENT_ID']}&redirect_uri=#{redirect_uri}&scope=repo" if !logged_in?
+  end
 
-    def logged_in?
-      !!session[:token]
-    end
+  def logged_in?
+    !!session[:token]
+  end
 end
