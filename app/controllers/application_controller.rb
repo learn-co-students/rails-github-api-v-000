a8acc9f7c782ed261bcefc81ebf601ec8a8a9c2b
lucketta <lucketta@umich.edu>
2017-08-12 21:32:56 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,16 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
+      if !logged_in
+        redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo"
+      end
     end
 
     def logged_in?
+       !!session[:token]
     end
 end
+
+# 537f95747ff12353026b
+#
+# 69697b1a57bd07678ba8abb2e86982c791be9ec9
