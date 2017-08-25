d22e3309a5869d00270891f8fd3ae7fae3522c23
github username apalski <github email address annette.drapalski@gmail.com>
2017-08-25 15:39:42 +1000

    Done.

--- spec/features/repos_spec.rb
+++ spec/features/repos_spec.rb
@@ -1,37 +1,42 @@
 require_relative '../spec_helper'
 
-describe "authentication" do
-  it "displays the username on the page" do
-    visit '/auth?code=20'
-    expect(page).to have_content 'your_username'
-  end
-end
+describe "Features" do
+  # describe "authentication" do
+  #   it "displays the username on the page" do
+  #     visit '/auth?code=20'
+  #     expect(page).to have_content 'your_username'
+  #   end
+  # end
 
-describe "visiting root" do
-  before :each do
-    page.set_rack_session(:token => "1")
-  end
+  describe "visiting root" do
+    before :each do
+      page.set_rack_session(:service => {"access_token" => 1})
+      page.set_rack_session(token: "1")
+    end
 
-  it "lists repos" do
-    visit '/'
-    expect(page).to have_content 'Repo 1'
-    expect(page).to have_content 'Repo 2'
-    expect(page).to have_content 'Repo 3'
-  end
-end
-
-describe "new repo form" do
-  before :each do
-    page.set_rack_session(:token => "1")
+    it "lists repos" do
+      visit '/'
+      expect(page).to have_content 'Repo 1'
+      expect(page).to have_content 'Repo 2'
+      expect(page).to have_content 'Repo 3'
+    end
   end
 
-  it "creates a new repo", :type => :request do
-    visit '/'
-    fill_in 'new-repo', with: 'a-new-repo'
-    click_button 'Create'
+  describe "new repo form" do
+    before :each do
+      page.set_rack_session(:service => {"access_token" => 1})
+      page.set_rack_session(token: "1")
+    end
 
-    expect(WebMock).to have_requested(:post, "https://api.github.com/user/repos").
-      with(:body => {name: "a-new-repo"}.to_json,
-      :headers => {'Authorization' => "token 1"})
+    it "creates a new repo", :type => :request do
+      stubbed = stub_request(:post, "https://api.github.com/user/repos").
+        with(body: {"name": "a-new-repo"}.to_json,
+        :headers => {'Authorization' => "token 1"})
+      visit root_path
+      fill_in 'new-repo', with: 'a-new-repo'
+      click_button 'Create'
+
+      expect(stubbed).to have_been_requested
+    end
   end
 end
