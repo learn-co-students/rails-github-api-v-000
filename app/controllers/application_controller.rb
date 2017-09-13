4ebaa7b7be8940c789232b6f5eeca79cf5ceae83
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-13 12:05:52 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,9 +8,10 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-      client_id = ENV['GITHUB_CLIENT_ID']
-      redirect_uri = CGI.escape("http://localhost:3000/auth")
-      github_url = "http://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=repo"
+      client_id = ENV['GITHUB_CLIENT']
+      #redirect_uri = CGI.escape("http://localhost:3000/auth")
+      github_url = "http://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
+
       redirect_to github_url unless logged_in?
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user, only: :Create
+  skip_before_action :authenticate_user, only: :create
 
   def create
-
-    redirect_uri = CGI.escape("http://localhost:3000/auth")
-
-    resp = Faraday.post("https://github.com/login/oauth/access_token", {client_id: ENV['GITHUB_CLIENT_ID'], redirect_uri: redirect_uri, client_secret: ENV['GITHUB_SECRET'], code: params[:code]}, {'Accept' => 'application/json'})
-    access_hash = JSON.parse(response.body)
+    resp = Faraday.post("https://github.com/login/oauth/access_token", {client_id: ENV['GITHUB_CLIENT'], client_secret: ENV['GITHUB_SECRET'], code: params[:code]}, {'Accept' => 'application/json'})
+    access_hash = JSON.parse(resp.body)
     session[:token] = access_hash["access_token"]
+
     user_response = Faraday.get("https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/JSON'})
     user_json = JSON.parse(user_response.body)
     session[:username] = user_json["login"]
+
     redirect_to root_path
   end
 end
