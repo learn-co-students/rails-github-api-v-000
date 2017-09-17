6a529b0b9f6d0f910fe7d179998fbe8d5e73d5bd
bshap93 <bshap93@gmail.com>
2017-09-16 19:29:39 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user
   private
 
     def authenticate_user
+      client_id = ENV['GITHUB_CLIENT_ID']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      github_url = "http://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
+      redirect_to github_url unless logged_in?
     end
 
     def logged_in?
+      !!session[:token]
     end
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,13 @@
 class RepositoriesController < ApplicationController
   def index
+    user = session[:username]
+    resp = Faraday.get "https://api.github.com/users/#{user}/repos"
+    body = JSON.parse(resp.body)
+    @repos = body
   end
 
   def create
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to '/'
   end
 end
