13ccd449bf8ca2145c21be525e4811de18f36b16
Pamela Bortnick <pbortnick@gmail.com>
2017-09-20 01:09:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def authenticate_user
-    end
+  def authenticate_user
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" unless logged_in?
+  end
 
     def logged_in?
+      !!session[:token]
     end
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,12 @@
 class RepositoriesController < ApplicationController
+  skip_before_action :authenticate_user
   def index
+    resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repositories = Kaminari.paginate_array(JSON.parse(resp.body)).page(params[:page])
   end
 
   def create
+    resp = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to root_path
   end
 end
