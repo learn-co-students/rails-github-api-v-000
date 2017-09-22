ccc7080f8480c4e52cb0bbac6a0c3ef8739b226d
lawrend <douglas_lawrence@mac.com>
2017-09-21 17:23:06 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,26 @@
 class RepositoriesController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user 
 
+  def reset_session
+      session[:token] = nil
+  end
+  def index
+      @resp = Faraday.get("https://api.github.com/user/repos") do |req|
+        req.headers['Accept'] = 'application/vnd.github.v3+json'
+        req.params['access_token'] = session[:token]
+      end
+      body = JSON.parse(@resp.body)
+      # name = body["login"] 
+      # @resp2 = Faraday.get("https://api.github.com/users/" + name + "/repos") do |req|
+      #     req.params['page'] = '2'
+      #     req.params['per_page'] = '10'
+      # end
+      # body2 = JSON.parse(@resp2.body)
+      @repos = body
+      # reset_session
+      @set = session[:token]
+  end
+  
   def create
   end
 end
