f151f1a02184b9f7ddd87858337c5c26dab161d9
curlywallst <nnoyes@comcast.net>
2017-09-26 14:57:56 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,28 +1,28 @@
 class RepositoriesController < ApplicationController
 
   def index
-    resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-      # resp = Faraday.get "https://api.github.com/user" do |req|
-      #   req.headers['Accept'] = 'application/json'
-      #   req.headers['Authorization'] = "token #{session[:token]}"
-      #
-      # end
-      #
-      # body = JSON.parse(resp.body)
-      # @login = body["login"]
-      #
+    # resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+      resp = Faraday.get "https://api.github.com/user" do |req|
+        req.headers['Accept'] = 'application/json'
+        req.headers['Authorization'] = "token #{session[:token]}"
+
+      end
+
+      body = JSON.parse(resp.body)
+      @login = body["login"]
+binding.pry
       # resp = Faraday.new(:url => 'http://www.example.com')
-      # # resp = Faraday.get "https://api.github.com/user/repos" do |req|
-      # #
-      # #   req.headers['Accept'] = 'application/json'
-      # #   req.headers['Authorization'] = "token #{session[:token]}"
-      # #
-      # # end
+      resp = Faraday.get "https://api.github.com/user/repos" do |req|
+
+        req.headers['Accept'] = 'application/json'
+        req.headers['Authorization'] = "token #{session[:token]}"
+
+      end
 binding.pry
       body = JSON.parse(resp.body)
 #binding.pry
       @items =  body.map{|y| y["name"]}
-      repos_url = body["repos_url"]
+      
 
 
     render 'index'
