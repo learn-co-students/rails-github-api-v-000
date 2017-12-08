ed2c2b0499772d59a9d86306b4d563d416c6d138
Joanna <jmgsese@gmail.com>
2017-12-08 16:21:58 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,10 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-      client_id = ENV['CLIENT_ID']
-      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&&scope=repo"
-      redirect_to github_url if logged_in?
+      # github_id = ENV['GITHUB_ID']
+      # github_url = "https://github.com/login/oauth/authorize?client_id=#{github_id}"
+      # redirect_to github_url if logged_in?
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
     end
 
     def logged_in?
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   def index
-   response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-   @repos_array = JSON.parse(response.body)
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos_array = JSON.parse(response.body)
   end
 
    def create
