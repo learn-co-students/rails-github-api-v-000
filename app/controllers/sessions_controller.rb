63a35fd1fee2c3de8edb54fc40bc30836f34f528
Scott Atkins <satkins16@gmail.com>
2017-12-26 13:31:08 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['CLIENT_ID']
+      req.params['client_secret'] = ENV['CLIENT_SECRET']
+      req.params['code'] = params[:code
+      req.params['redirect_uri'] = "http://localhost:3000/auth"]
   end
-  
+
 end
