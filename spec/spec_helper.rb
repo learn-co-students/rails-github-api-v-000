f87302efb6a6599e913bc50478713cf8ecfc9de0
marielfrank <marielbeth@gmail.com>
2018-01-02 16:45:33 -0500

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,13 +12,13 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.before(:each) do
-    stub_request(:get, "https://api.github.com/user/repos").
-      with(:headers => {'Authorization'=>'token 1'}).
+    stub_request(:get, "https://api.github.com/user/repos?affiliation=owner").
+      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'token 1', 'User-Agent'=>'Faraday v0.9.1'}).
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:post, "https://github.com/login/oauth/access_token").
-      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20"},
-      :headers => {'Accept'=>'application/json'}).
+      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20", "redirect_uri"=>"http://localhost:3000/auth"},
+      :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'}).
       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
 
     stub_request(:get, "https://api.github.com/user").
