62a5a1fe5d87b31d621b9227a0fcd445bd018215
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-02 23:21:22 -0800

    clear session was the key

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,17 +1,5 @@
 class RepositoriesController < ApplicationController
 
-  # def index
-  #   page_num = 1
-  #   resp = Faraday.get "https://api.github.com/user/repos?per_page=100&page=#{page_num}", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-  #   @user_repos = JSON.parse(resp.body)
-  #   while !JSON.parse(resp.body).empty?
-  #     page_num +=1
-  #     resp = Faraday.get "https://api.github.com/user/repos?per_page=100&page=#{page_num}", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-  #     @user_repos.push(*(JSON.parse(resp.body)) unless resp.body.empty?
-  #   end
-  #   @user_repos
-  # end
-
   # def index #version that lists all pages fully
   #   @repos = []
   #   page_num = 1
@@ -28,18 +16,46 @@ class RepositoriesController < ApplicationController
     @repos = JSON.parse(resp.body)
   end
 
-  def create
-    conn = Faraday.new("https://api.github.com")
+#   def index
+#   response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+#   @repos_array = JSON.parse(response.body)
+# end
 
-  	resp = conn.post do |req|
-  		req.url '/user/repos'
-  		req.headers['Content-Type'] = 'application/json'
-  		req.headers['Authorization'] = "token #{session[:token]}"
-  		req.body = {'name': params[:name]}.to_json
-  	end
-    # binding.pry
-    redirect_to root_path
+  # def create
+  #   conn = Faraday.new("https://api.github.com")
+  #
+  # 	resp = conn.post do |req|
+  # 		req.url '/user/repos'
+  # 		req.headers['Content-Type'] = 'application/json'
+  # 		req.headers['Authorization'] = "token #{session[:token]}"
+  # 		req.body = {'name': params[:name]}.to_json
+  # 	end
+  #   # binding.pry
+  #   redirect_to root_path
+  # end
+
+  # def create
+  #   conn = Faraday.new(url: 'https://api.github.com/user/repos')
+  #   dummy = conn.post do |req|
+  #     req.headers = { Accept: 'application/json',
+  #                     Authorization: "token #{session[:token]}" }
+  #     req.body = { name: params[:name] }.to_json
+  #   end
+  #   binding.pry
+  #   redirect_to root_path
+  # end
+
+  def create
+    response = Faraday.post "https://api.github.com/user/repos",{ name: params[:name] }.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    binding.pry
+    redirect_to '/'
   end
+
+  # def create
+  #   response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+  #   binding.pry
+  #   redirect_to '/'
+  # end
 
   # def create
   #   result = RestClient.post('https://api.github.com/user/repos', {name: params[:name], :authorization => "Token token=#{session[:token]}"})
