c88e126a37745717c3fbd121e320041cb18201fb
Adam Tropp <adt6261@gmail.com>
2018-01-13 20:32:45 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,4 +1,5 @@
 class RepositoriesController < ApplicationController
+
   def index
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    resp = Faraday.get("https://github.com/login/oauth/authorize") do |req|
-      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
-    end
+    resp = Faraday.post "https://github.com/login/oauth/access_token",
+      {client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET'],
+      code: params[:code]}, {'Accept' => 'application/json'}
     body = JSON.parse(resp.body)
     session[:token] = body["access_token"]
     redirect_to root_path
