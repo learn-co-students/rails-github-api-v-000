50a4dc8160021a424b79dc6070bad633d2be408d
dbfarms <matt.hunger@gmail.com>
2018-01-16 19:13:07 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,42 @@
 class RepositoriesController < ApplicationController
   def index
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos_array = JSON.parse(response.body)
+    #binding.pry
+  end
+
+ def create
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    binding.pry
+    redirect_to '/'
+  end
+end
+
+
+
+=begin
+
+class RepositoriesController < ApplicationController
+  def index
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos_array = JSON.parse(response.body)
   end
 
   def create
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to '/'
   end
-end
+
+
+
+
+    #binding.pry
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      #req.headers['Accept'] = 'application/json'
+      req.params['oauth_token'] = session[:token]
+    end
+    binding.pry
+    @repos = JSON.parse(resp.body)
+    
+    @name = params[:name].to_json
+=end 
