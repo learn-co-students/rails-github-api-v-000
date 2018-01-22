1d06b2ffd22dc9c4ca0319f00945daea89b29368
dkStephanos <stephanos@goldmail.etsu.edu>
2018-01-22 18:59:13 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,31 @@
 class RepositoriesController < ApplicationController
+  before_action :authenticate_user
+
   def index
+    resp = Faraday.get "https://api.github.com/user", { access_token:  session[:token] },
+      { 'Accept' => 'application/json'}
+
+    body = JSON.parse(resp.body)
+
+    #binding.pry
+
+    @username = body["login"]
+
+    resp = Faraday.get "https://api.github.com/users/#{@username}/repos", { access_token:  session[:token] },
+      { 'Accept' => 'application/json'}
+
+    body = JSON.parse(resp.body)
+
+    @repo_names = []
+
+    body.each do |repo|
+      @repo_names << repo["name"]
+    end
+
+
   end
 
   def create
   end
 end
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post "https://github.com/login/oauth/access_token", { client_id:  ENV['GITHUB_CLIENT_ID'],
+       client_secret:  ENV['GITHUB_CLIENT_SECRET'],
+       code:  params[:code] },
+      { 'Accept' => 'application/json'}
+
+    body = JSON.parse(resp.body)
+
+    #binding.pry
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
+
