1d93a078b480eae9d2d65e4c68080caf83b9663e
jlmack13 <jennimacklin@gmail.com>
2018-01-24 11:41:49 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user
 
   private
 
     def authenticate_user
+      client_id = ENV['GITHUB_CLIENT_ID']
+      scope = 'repo'
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=#{scope}" if !logged_in?
     end
 
     def logged_in?
+      !!session[:token]
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
+      req.headers['Accept'] = 'application/json'
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params[:code]
+    end
+    body = JSON.parse(resp.body)
+    access_token = body["access_token"]
+    session[:token] = access_token
+
+    user_resp = Faraday.get('https://api.github.com/user') do |req|
+      req.headers = {'Authorization': "token#{session[:token]}", 'Accept': 'application/json'}
+    end
+
+    @user_info = JSON.parse(user_resp.body)
+    session[:username] = @user_info["login"]
+    redirect_to root_path
   end
+
 end
