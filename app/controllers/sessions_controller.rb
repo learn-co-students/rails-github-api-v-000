87a1844f13c08ef18ecf1c1c951a9adb260a289d
Heather You <heatherhyou@gmail.com>
2018-01-30 16:14:24 -0500

    Webmock not stopping tests from accessing page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params[:code]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+
+    user_resp = Faraday.get("https://api.github.com/user") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    user_json = JSON.parse(user_resp.body)
+    session[:username] = user_json["login"]
+
+    redirect_to root_path
   end
 end
