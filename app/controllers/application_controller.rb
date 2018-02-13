af8c05b2a43c18303146f1634790e084ffad4056
jss530 <jsifre01@gmail.com>
2018-02-12 19:26:50 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
 
-  private
+private
 
   def authenticate_user
-    client_id = ENV['GITHUB_OAUTH_CLIENT_ID']
+    client_id = ENV['GITHUB_CLIENT_ID']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
     github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
     redirect_to github_url unless logged_in?
   end
 
-    def logged_in?
-      !!session[:token]
-    end
+  def logged_in?
+    !!session[:token]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,18 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    resp = Faraday.get("https://github.com/login/oauth/authorize") do |req|
-      req.params['client_id'] = ENV['GITHUB_OAUTH_CLIENT_ID']
-      req.params['client_secret'] = ENV['GITHUB_OAUTH_CLIENT_SECRET']
-      req.params['code'] = params[:code]
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.headers = {"Accept": 'application/json'}
+      req.body = {client_id: ENV['GITHUB_CLIENT_ID'], client_secret: ENV['GITHUB_CLIENT_SECRET'],
+        redirect_uri: "http://localhost:3000/auth", code: params[:code]}
     end
-    response = resp.to_json
-    body = JSON.parse(response)
+
+    body = JSON.parse(resp.body)
     session[:token] = body["access_token"]
+
+    user = Faraday.get("https://api.github.com/user?access_token=#{session[:token]}")
+    user_info = JSON.parse(user.body)
+    session[:username] = user_info["login"]
     redirect_to root_path
   end
 end
