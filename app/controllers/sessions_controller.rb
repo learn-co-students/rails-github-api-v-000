32e552bb0508f792855f97d17c7a60148712788b
arwehrman <arwehrman@gmail.com>
2018-02-16 21:33:33 -0500

    all tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user, only: :create
+
   def create
-  end
+    resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT_ID"], client_secret: ENV["GITHUB_CLIENT_SECRET"], code: params[:code]}, {'Accept' => 'application/json'}
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+
+    user_resp = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+
+    body = JSON.parse(user_resp.body)
+    session[:username] = body["login"]
+      redirect_to '/'
+    end
 end
