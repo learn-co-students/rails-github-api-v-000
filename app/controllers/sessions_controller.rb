a5fdcba272dfc02403022a609aeb8c72eb6505ad
github username <schun08@gmail.com>
2018-03-04 18:41:42 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    # post request to GitHub with user's code
+    resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV['GITHUB_CLIENT_ID'], client_secret: ENV['GITHUB_SECRET'], code: params[:code]}, {'Accept' => 'application/json'}
+
+    body = JSON.parse(resp.body)
+    # set the token
+    session[:token] = body["access_token"]
+
+    # log in to get user info
+    user = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}"}
+
+    user_info = JSON.parse(user.body)
+    # store the username
+    session[:username] = user_info["login"]
+
+    redirect_to root_path
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,8 @@ RSpec.configure do |config|
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:post, "https://github.com/login/oauth/access_token").
-      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20"},
-      :headers => {'Accept'=>'application/json'}).
+      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_SECRET"], "code"=>"20"},
+        :headers =>  {'Accept'=>'application/json'}).
       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
 
     stub_request(:get, "https://api.github.com/user").
@@ -27,8 +27,8 @@ RSpec.configure do |config|
 
     stub_request(:post, "https://api.github.com/user/repos").
       with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
-      :headers => {'Authorization'=>'token 1'}).
-      to_return(:status => 201, :body => "", :headers => {})
+           :headers => {'Authorization'=>'token 1'}).
+      to_return(:status => 200, :body => "", :headers => {})
   end
 end
 
