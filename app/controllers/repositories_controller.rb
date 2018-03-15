896b7984467acae6400f3d25da7ff6f89ddabffb
Igor Eskin <github email address>
2018-03-14 23:16:28 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,21 @@
 class RepositoriesController < ApplicationController
 
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers['Accept'] = 'application/json'
+      req.headers["Authorization"] = "token #{session[:token]}"
+    end
+    @repos = JSON.parse(resp.body)
   end
 
   def create
-    
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers["Authorization"] = "token #{session[:token]}"
+      req.headers["Accept"] = 'application/json'
+      req.body = { name: params[:name] }.to_json
+    end
+
+    redirect_to root_path
   end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,9 @@ class SessionsController < ApplicationController
   def create
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
       req.body = {
-      client_id: ENV['CLIENT_ID'],
-      client_secret: ENV['CLIENT_SECRET'],
+      client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET'],
       code: params[:code]
-      # req.params['redirect_uri']: "http://localhost:3000/auth"
       }
       req.headers['Accept'] = 'application/json'
     end
@@ -15,12 +14,13 @@ class SessionsController < ApplicationController
     body = JSON.parse(resp.body)
     session[:token] = body["access_token"]
 
-    resp = Faraday.get("https://api.github.com/user") do |req|
+    user_resp = Faraday.get("https://api.github.com/user") do |req|
       req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
     end
 
-    body = JSON.parse(resp.body)
-    session[:username] = body["login"]
+    user_json = JSON.parse(user_resp.body)
+    session[:username] = user_json["login"]
 
     redirect_to root_path
   end
