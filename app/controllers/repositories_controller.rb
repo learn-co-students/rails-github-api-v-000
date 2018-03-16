087e5c75097b6cb814932ee6412f939c144561e5
BuffaloRob <peckham.rob@gmail.com>
2018-03-16 14:48:47 -0400

    WIP

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,10 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.params['oauth_token'] = session[:token]
+    end
+
+  @repos = JSON.parse(resp.body)
   end
 
   def create
