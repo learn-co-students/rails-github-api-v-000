6b8a6ea2c7ce4d2e701ab86445a8530bf61b7915
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-21 13:40:47 +0100

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.before(:each) do
+
     stub_request(:get, "https://api.github.com/user/repos").
       with(:headers => {'Authorization'=>'token 1'}).
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
@@ -20,6 +21,12 @@ RSpec.configure do |config|
       with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20"},
       :headers => {'Accept'=>'application/json'}).
       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
+
+    # stub_request(:post, "https://github.com/login/oauth/access_token").
+    #   with(:body => {"client_id"=>"", "client_secret"=>"", "code"=>"20"},
+    #   :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.9.1'}).
+    #   to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
+
 
     stub_request(:get, "https://api.github.com/user").
       with(:headers => {'Authorization'=>'token 1'}).
