7a82f003fadda9fc683da3df193274435e464bf1
chrisjamesr <chrisjames.r@gmail.com>
2018-03-28 19:54:25 -0400

    save working branch

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,16 @@
 class RepositoriesController < ApplicationController
+  
   def index
+    conn = Faraday.new(:url =>"https://api.github.com")
+    get_repos = conn.get "/users/#{session[:username]}/repos",{'Authorization' => "token session[:token]", 'Accept' => 'application/json'}
+    repo_response = JSON.parse(get_repos. body)
+    # raise get_repos.inspect
+    @repos = repo_response.collect{|r| r}
   end
 
   def create
+    
   end
 end
+
+# "access_token":"e72e16c7e42f292c6912e7710c838347ae178b4a", "scope":"repo,gist", "token_type":"bearer"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user,  only: :create
+
   def create
+    state = "GOCVKFbt4httKVmlaBbBqfY32TE"
+    auth_token = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"], code: params[:code]}, {'Accept' => 'application/json'}
+    #   req.headers['Accept'] = "application/json"
+    #   req['Content-Type'] = "application/json"
+    #   req['User-Agent'] = 'Faraday v0.9.1']
+    #   req.params = {
+    #     'client_id': ENV['GITHUB_CLIENT_ID'],
+    #     'client_secret': ENV['GITHUB_CLIENT_SECRET'],
+    #     'code': params[:code],
+    #     'state': params[:state]
+    #   }
+      # end
+    auth_response = JSON.parse(auth_token.body)
+    session[:token] = auth_response["access_token"]    
+    
+
+    user_info = Faraday.get "https://api.github.com/user",{},{'Authorization' => "token session[:token]", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_info.body)
+    session[:username] = user_json["login"]
+
+    redirect_to "/"
   end
+  
 end
