7f1bbcbd6a85c23c82f2e5193d06bae71dc3e286
Donnadieu <donnadieu.86@gmail.com>
2018-04-04 13:30:29 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,7 @@
 class RepositoriesController < ApplicationController
   def index
-    resp = Faraday.get "https://api.github.com/user?access_token=270804b98a022f51425a17110c07931a399bbc2d"
-    user_info = JSON.parse(resp.body)
-    @repos = JSON.parse((Faraday.get "#{user_info['repos_url']}").body)
-    binding.pry
+    resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos = JSON.parse(resp.body)
   end
 
   def create
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,10 @@ class SessionsController < ApplicationController
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT_ID"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
     body = JSON.parse(response.body)
     session[:token] = body["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
     redirect_to root_path
   end
 end
