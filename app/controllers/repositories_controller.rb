7da2d57659102807e2c397d4d88c8e983cd01f54
Sam Ojling <s.ojling@gmail.com>
2018-04-09 16:51:37 +0200

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,20 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.headers['Accept'] = 'application/json'
+    end
+    @repos = JSON.parse(resp.body)
   end
 
   def create
+    resp = Faraday.post('https://api.github.com/user/repos') do |req|
+      req.headers = {
+        'Accept' => 'application/json',
+        'Authorization' => "token #{session[:token]}"
+      }
+      req.body = {'name' => params[:name]}
+    end
+    redirect_to root_path
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    oauth_resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
+      req.headers = {'Accept' => 'application/json'}
+      req.body = {
+        'client_id' => ENV['GITHUB_CLIENT_ID'],
+        'client_secret' => ENV['GITHUB_CLIENT_SECRET'],
+        'code' => params[:code]
+      }
+    end
+    oauth_body = JSON.parse(oauth_resp.body)
+    session[:token] = oauth_body["access_token"]
+
+    user_resp = Faraday.get('https://api.github.com/user') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    user_body = JSON.parse(user_resp.body)
+    session[:username] = user_body["login"]
+
+    redirect_to root_path
   end
 end
