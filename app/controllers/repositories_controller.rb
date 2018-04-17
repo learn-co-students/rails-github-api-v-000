fcf4b7fc07d0437fb878cf1e234ec26c26acaf90
Hoon Lee <yl.hoony@gmail.com>
2018-04-17 13:11:59 -0700

    set oauth

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,23 @@
 class RepositoriesController < ApplicationController
   def index
+
   end
 
   def create
+    # binding.pry
+    # resp = Faraday.post do |req|
+    #   req.url "https://api.github.com/user/repos"
+    #   req.headers['Authorization'] = "token #{session[:token]}"
+    #   req.headers['Accept'] = 'application/json'
+      
+    #   req.body = { name: params[:name] }
+    #   req.body = { name: "testtest" }.to_json
+    # end
+
+    # redirect_to '/'
+
+    response = Faraday.post "https://api.github.com/user/repos", { name: "testtest" }, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    binding.pry
+    redirect_to '/'
   end
 end
