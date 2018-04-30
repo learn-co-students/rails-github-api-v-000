3d6ba7fd720df33f2eb1c96c1d0d97c1a81729c6
github username <github email address>
2018-04-30 14:48:29 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ RSpec.configure do |config|
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:post, "https://github.com/login/oauth/access_token").
-      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20"},
+      with(:body => {"client_id"=> ENV["CLIENT_ID"], "client_secret"=> ENV["CLIENT_SECRET"], "code"=>"20"},
       :headers => {'Accept'=>'application/json'}).
       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
 
@@ -31,5 +31,45 @@ RSpec.configure do |config|
       to_return(:status => 201, :body => "", :headers => {})
   end
 end
-
+# WebMock.allow_net_connect!
 WebMock.disable_net_connect!(allow_localhost: true)
+
+# ENV["RAILS_ENV"] ||= 'test'
+# require File.expand_path("../../config/environment", __FILE__)
+# require 'rspec/rails'
+#
+# require 'capybara/dsl'
+# require 'capybara/rails'
+# require 'capybara/rspec'
+# require 'webmock/rspec'
+# require 'rack_session_access/capybara'
+#
+# RSpec.configure do |config|
+#   config.include Capybara::DSL
+#
+#   config.before(:each) do
+#     stub_request(:post, "https://github.com/login/oauth/access_token?client_id=c83bf4cf929dad0a0168&client_secret=64930adbb59c92187c52fc4bbfc3f088f1551905&code=20").
+#    with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Length'=>'0', 'User-Agent'=>'Faraday v0.9.1'}).
+#    to_return(:status => 200, :body => "", :headers => {})
+#
+#     stub_request(:get, "https://api.github.com/user/repos").
+#       with(:headers => {'Authorization'=>'token 1'}).
+#       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
+#
+#     stub_request(:post, "https://github.com/login/oauth/access_token").
+#       with(:body => {"client_id"=> ENV["CLIENT_ID"], "client_secret"=> ENV["CLIENT_SECRET"], "code"=>"20"},
+#       :headers => {'Accept'=>'application/json'}).
+#       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
+#
+#     stub_request(:get, "https://api.github.com/user").
+#       with(:headers => {'Authorization'=>'token 1'}).
+#       to_return(:status => 200, :body => {"login"=>"your_username"}.to_json, :headers => {})
+#
+#     stub_request(:post, "https://api.github.com/user/repos").
+#       with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
+#       :headers => {'Authorization'=>'token 1'}).
+#       to_return(:status => 201, :body => "", :headers => {})
+#   end
+# end
+#
+# WebMock.disable_net_connect!(allow_localhost: true)
