151927a95314f06c8bce286f9ca52c576ba4c908
Chris Boujoukos <github email address>
2018-05-03 13:03:27 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post('https://github.com/login/oauth/access_token', {client_id: ENV['GITHUB_ID'], client_secret: ENV['GITHUB_SECRET'], code: params['code']})
+    
+    session[:token] = params['access_token']
+    redirect_to root_path
   end
 end
