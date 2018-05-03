b23611c5335f0b4c2d5ee3c7628f5a0a27e5cf52
terryblue99 <terryblue@comcast.net>
2018-05-03 16:44:34 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,6 +2,16 @@ class RepositoriesController < ApplicationController
 	
   def index
   	
+  	@repos_response = Faraday.get "https://api.github.com/user/repos" do |req|
+		req.params['oauth_token'] = session[:token]
+	end
+
+	if @repos_response.success?
+		@repos = JSON.parse(@repos_response.body)
+	else
+		@error = body["meta"]["errorDetail"]
+	end
+	
   end
 
   def create
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,15 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.before(:each) do
-    stub_request(:get, "https://api.github.com/user/repos").
-      with(:headers => {'Authorization'=>'token 1'}).
-      to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
+
+    stub_request(:get, "https://api.github.com/user/repos?oauth_token=1").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v0.9.1'
+           }).
+         to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:get, "https://github.com/login/oauth/access_token?client_id=5dd223d3dd78adf88b36&client_secret=6c2d719cb76f482aa5d099944a7306c5db97b61f&code=20&headers%5BAccept%5D=application/json&redirect_uri=http://localhost:3000/auth").
      with(
@@ -48,6 +54,7 @@ RSpec.configure do |config|
       :headers => {'Authorization'=>'token 1'}).
       to_return(:status => 201, :body => "", :headers => {})
   end
+  
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
