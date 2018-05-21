5a4f4e780a969a3d2ce51983fde3f26bb1930db4
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-21 21:38:15 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,28 @@
 class RepositoriesController < ApplicationController
   def index
+    resp1 = Faraday.get("https://api.github.com/user") do |req|
+      req.headers = {
+        Authorization: "token #{session[:token]}"
+      }
+    end
+    body1 = ActiveSupport::JSON.decode(resp1.body)
+    @username = body1["login"]
+
+    resp2 = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers = {
+        Authorization: "token #{session[:token]}"
+      }
+    end
+    @repos = ActiveSupport::JSON.decode(resp2.body)
   end
 
   def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers = {
+        Authorization: "token #{session[:token]}"
+      }
+      req.body = ActiveSupport::JSON.encode({"name" => params[:name]})
+    end
+    render "index"
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.headers = {
+        Accept: 'application/json'
+      }
+      req.body = {
+        client_id: ENV['GITHUB_CLIENT_ID'],
+        client_secret: ENV['GITHUB_CLIENT_SECRET'],
+        code: params['code'],
+      }
+    end
+    access_token = ActiveSupport::JSON.decode(resp.body)["access_token"]
+    session[:token] = access_token
+    redirect_to root_path
   end
+
 end
