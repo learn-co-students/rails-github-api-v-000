11ab014d7d613a2ee64f55f86e7305a489095a75
Christina <github email address>
2018-05-21 19:52:22 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,21 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.headers['Accept'] = 'application/json'
-      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
-      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
-      req.params['code'] = params[:code]
+            req.body = {"client_id": ENV['GITHUB_CLIENT_ID'],
+            "client_secret": ENV['GITHUB_CLIENT_SECRET'],
+            "code": params[:code]}
+            req.headers['Accept'] = 'application/json'
     end
+      #syntax commented below did not work and had an error with username - code above does work
+    #@resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+    #@  req.headers['Accept'] = 'application/json'
+    #@  req.body['client_id'] = ENV['GITHUB_CLIENT_ID']
+    #@  req.body['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+    #@  req.body['code'] = params[:code]
+    #@end
     body = JSON.parse(resp.body)
     access_token = body["access_token"]
-    #session[:token] = access_token
+    session[:token] = access_token
 
     user_resp = Faraday.get('https://api.github.com/user') do |req|
       req.headers['Authorization'] = "token #{access_token}"
