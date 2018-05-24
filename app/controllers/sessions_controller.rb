802f9e1e68f1ff95757f1afde2892196b4b40c3c
jacobsilver2 <jacobsilver2@mac.com>
2018-05-24 10:51:47 -0400

    dfd

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,6 @@ class SessionsController < ApplicationController
     access_hash = JSON.parse(response.body)
     session[:token] = access_hash["access_token"]
 
-    # user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    # user_json = JSON.parse(user_response.body)
-    # session[:username] = user_json["login"]
-
     redirect_to root_path
   end
 
