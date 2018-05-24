15b9e896c8d1d487f974f247f7069b601c3d5308
jacobsilver2 <jacobsilver2@mac.com>
2018-05-24 09:36:35 -0400

    first commit

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,11 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get("https://api.github.com/user?access_token=") do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    @repo = JSON.parse(resp.body)
   end
 
   def create
