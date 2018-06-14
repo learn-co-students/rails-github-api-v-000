c00cc914081a50c7bc967c2a368cfe65d49d529f
catalanowebdevelopment <catalano.work@gmail.com>
2018-06-14 13:24:55 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,8 @@
 class RepositoriesController < ApplicationController
   def index
+      response = Faraday.get("https://api.github.com/user/repos") do |req|
+          binding.pry
+      end
   end
 
   def create
