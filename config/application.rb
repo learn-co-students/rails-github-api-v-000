3a28a8944b9309cb89b2df29600dda3a94b723af
github username <github email address>
2018-06-13 22:25:10 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module GithubDemo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.web_console.development_only = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
