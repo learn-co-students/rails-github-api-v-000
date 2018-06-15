93f1791c1578e8120b3c5e80edad2d50a7077670
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-15 20:26:40 +0100

    Done.

--- spec/features/repos_spec.rb
+++ spec/features/repos_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../spec_helper'
 
-describe "authentication" do
-  it "displays the username on the page" do
+describe 'authentication' do
+  it 'displays the username on the page' do
     visit '/auth?code=20'
     expect(page).to have_content 'your_username'
   end
 end
 
-describe "visiting root" do
+describe 'visiting root' do
   before :each do
-    page.set_rack_session(:token => "1")
+    page.set_rack_session(token: '1')
   end
 
-  it "lists repos" do
+  it 'lists repos' do
     visit '/'
     expect(page).to have_content 'Repo 1'
     expect(page).to have_content 'Repo 2'
@@ -20,18 +20,18 @@ describe "visiting root" do
   end
 end
 
-describe "new repo form" do
+describe 'new repo form' do
   before :each do
-    page.set_rack_session(:token => "1")
+    page.set_rack_session(token: '1')
   end
 
-  it "creates a new repo", :type => :request do
+  it 'creates a new repo', type: :request do
     visit '/'
     fill_in 'new-repo', with: 'a-new-repo'
     click_button 'Create'
 
-    expect(WebMock).to have_requested(:post, "https://api.github.com/user/repos").
-      with(:body => {name: "a-new-repo"}.to_json,
-      :headers => {'Authorization' => "token 1"})
+    expect(WebMock).to have_requested(:post, 'https://api.github.com/user/repos')
+      .with(body: { name: 'a-new-repo' }.to_json,
+            headers: { 'Authorization' => 'token 1' })
   end
 end
