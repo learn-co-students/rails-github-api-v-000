4558f038a9386b1b3fbe85c842bb178606fc5df5
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-15 10:50:29 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,10 +8,10 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-      # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
-      # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['CLIENT_ID']}&scope=repo" if !logged_in?
     end
 
-    def logged_in?
-    end
+  def logged_in?
+    !!session[:token]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+  # skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+      # req.params['client_id'] = ENV['CLIENT_ID']
+      # req.params['client_secret'] = ENV['CLIENT_SECRET']
+      # req.params['code'] = params[:code]
+      req.body = {client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"],code: params[:code]}
+      req.headers = {'Accept' => 'application/json'}
+    end
+binding.pry
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+
+    # response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+
+    redirect_to root_path
   end
 end
