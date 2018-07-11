f0222af6bfc777911e8a6e9b5b209f34c9cd82e8
Sean Oughton <seanoughton@gmail.com>
2018-07-11 16:46:46 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'faraday'
 # Dot-env
 gem 'dotenv-rails'
 
+
 #Testing
 group :test do
   gem 'rspec-rails'
@@ -57,4 +58,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user, only: :create
+
   def create
+
+    #resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+    #req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
+    #req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+    #req.params['grant_type'] = 'authorization_code'
+    #req.params['redirect_uri'] = "http://localhost:3000/auth"
+    #req.params['code'] = params[:code]
   end
+
+  #body = JSON.parse(resp.body)
+  #session[:token] = body["access_token"]
+  #redirect_to root_path
 end
