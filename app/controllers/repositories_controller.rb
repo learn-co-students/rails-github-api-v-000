ec382782ea3e0960326659683a2c861efd3cf09f
Chad Montoya <chadmontoya21@yahoo.com>
2018-07-12 18:30:45 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,26 +1,27 @@
 class RepositoriesController < ApplicationController
   def index
-    
-    resp = Faraday.get("https://api.github.com/user/repos") do |req|
-      req.headers = {'Authorization': 'token OAUTH-TOKEN'}
-      req.params['affiliation'] = 'owner'
-      req.params['access_token'] = session[:token] 
+
+    user_info = Faraday.get("https://api.github.com/user") do |req|
+      req.headers = {'Authorization' => "token #{session[:token]}"}
     end
-    
+
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      # binding.pry
+      req.headers = {'Authorization' => "token #{session[:token]}"}
+    end
+
     @repos = JSON.parse(resp.body)
-    @username = @repos.first['owner']['login']
+    @user = JSON.parse(user_info.body)
   end
 
   def create
     resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.headers = {'Authorization' => "token #{session[:token]}"}
       req.body = {
-        'client_id': ENV['CLIENT_ID'],
-        'client_secret': ENV['CLIENT_SECRET'],
-        'access_token': session['token'],
-        'name': params['name']
+        'name' => params['name']
       }.to_json
     end
-    binding.pry
+
     redirect_to root_path
   end
 end
