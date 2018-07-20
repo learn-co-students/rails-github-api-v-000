7b93b216d58356eeb1466e4f52ac277814159b85
Jirles <emily.jirles@gmail.com>
2018-07-19 22:42:35 -0400

    Set up index action with Github repo API call

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,18 @@
 class RepositoriesController < ApplicationController
   
   def index
+    @resp = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.headers['Accept'] = 'application/json'
+      req.params['access_token'] = session[:token]
+      req.params['affiliation'] = 'owner'
+      req.params['sort'] = 'updated'
+    end 
+    parsed_json = JSON.parse(@resp.body)
+    if @resp.success?
+      @repos = parsed_json
+    else
+      @error = "Something went wrong."
+    end
   end
 
   def create
