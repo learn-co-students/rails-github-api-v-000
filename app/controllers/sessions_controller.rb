fabb5a3c2a2c9cd22265c6c1cdd72f23112946dc
Jirles <emily.jirles@gmail.com>
2018-07-19 22:02:09 -0400

    Setup User sign-in through Github OAuth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
+    redirect_to root_path if params['state'] != ENV['GITHUB_STATE']
+    @resp = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
+      req.headers['Accept'] = 'application/json'
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['code'] = params['code']
+      req.params['state'] = ENV['GITHUB_STATE']
+    end
+    parsed_json = JSON.parse(@resp.body)
+    session[:token] = parsed_json['access_token']
+    redirect_to root_path
   end
 end
