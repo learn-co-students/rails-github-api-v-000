ba4397d4da8065d81170f12dfb817c1535f1e98b
Jirles <emily.jirles@gmail.com>
2018-07-19 23:18:05 -0400

    Add create repo action, untested

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -16,6 +16,14 @@ class RepositoriesController < ApplicationController
   end
 
   def create
+    resp = Faraday.post 'https://api.github.com/user/repos' do |req|
+      req.headers['Accept'] = 'application/json'
+      req.body = "{ 'access_token': #{session[:token]} }"
+      req.body = "{ 'name': #{params[:name]} }"
+      req.body = "{ 'private': false }"
+      req.body = "{ 'auto_init': true }"
+    end 
+    redirect_to root_path
   end
 
 end
