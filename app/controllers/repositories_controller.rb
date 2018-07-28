3f8a69d95119278c868cfcb4f310fdf34246163c
Raiet <raietsaeed@gmail.com>
2018-07-28 01:22:11 +0000

    working? code pre-comment removal and AAQ httpconnection issue

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,33 @@
 class RepositoriesController < ApplicationController
   def index
+    client_id = ENV['client_id']
+    client_secret = ENV['client_secret']
+
+    @resp = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['access_token'] = session[:token]
+    end
+
+    body = JSON.parse(@resp.body)
+    # puts "BODY = #{body.length}"
+    # body.each do |repo|
+    #   puts "Repo = #{repo}"
+    #   puts "Repo id = #{}"
+    #   puts "name = #{repo['full_name']}"
+    # end
+    puts "USER = #{body[1]['owner']['login']}"
+    @username = body[1]['owner']['login']
+    @repos = body
   end
 
   def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.params['name'] = params[:name]
+      req.headers['Authorization'] = params[:authenticity_token]
+      # req.headers['Authorization'] = session[:token]
+    end
+    
+    redirect_to root_path
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
   def create
+    # puts "REACHED AUTH CREATE ROUTE!"
+    # puts "Params = #{params}"
+    
+    authResponse = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['client_id']
+      req.params['client_secret'] = ENV['client_secret']
+      req.params['state'] = 'thisisarandomscopeforauthorization'
+      req.params['code'] = params["code"]
+      req.headers['Accept'] = 'application/json'
+    end
+    # puts "AUTH RESPONSE = #{authResponse}"
+    # puts "AUTH BODY = #{authResponse.body}"
+    auth = JSON.parse(authResponse.body)
+    # puts "AUTH = #{auth}"
+    # puts "AUTH TOKEN = #{auth['access_token']}"
+    session[:token] = auth['access_token']
+    redirect_to root_path
   end
 end
