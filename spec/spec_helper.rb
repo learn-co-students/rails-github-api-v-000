57f0fffa20e1d7c58b8364857d5de427dc317677
Michael Hirschberger <mlhirschberger@gmail.com>
2018-08-01 13:46:09 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,12 @@ RSpec.configure do |config|
       with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
       :headers => {'Authorization'=>'token 1'}).
       to_return(:status => 201, :body => "", :headers => {})
-  end
+
+    stub_request(:post, "https://github.com/login/oauth/access_token").
+      with(:body => {"client_id"=>"4c89b67e4409878881f3", "client_secret"=>"", "code"=>"20"},
+            :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.9.1'}).
+      to_return(:status => 200, :body => "", :headers => {})
+    end
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
