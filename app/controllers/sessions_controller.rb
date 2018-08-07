eb394dd127ec259a203d1bd48c8e8af0f18142b7
fonzmp <fonzmp@gmail.com[color]>
2018-08-06 21:52:36 -0400

    safety commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user
+
   def create
+    resp = Faraday.post('https://github.com/login/oauth/access_token', 
+    {
+      :client_id => ENV['CLIENT_ID'],
+      :client_secret => ENV['CLIENT_SECRET'],
+      :code => params['code']
+    },
+    :accept => :json)
+    
+    binding.pry
+    # @user = JSON.parse(resp)
+    # sessions[:token] = @user[:access_token]
+    
+    redirect_to root_path
   end
+
 end
