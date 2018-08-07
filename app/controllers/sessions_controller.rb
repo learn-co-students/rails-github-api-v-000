86f51dc656ef787948580942421a976e1d732522
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 15:35:13 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-   skip_before_action :authenticate_user, only: :create
+   skip_before_action :authenticate_user
    def create
      resp = Faraday.post('https://github.com/login/oauth/access_token', {
        :client_id => ENV['CLIENT_ID'],
