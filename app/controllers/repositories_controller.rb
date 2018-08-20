6e032a445efc8119e96efb857e7015d18a8b53f7
github username <purnima.nyc@gmail.com>
2018-08-20 12:38:13 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,25 @@
 class RepositoriesController < ApplicationController
-  
+
+  skip_before_action :authenticate_user
+
   def index
+    resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.params['oauth_token'] = session[:token]
+      #req.params['v'] = '3'
+    end
+    @repos = JSON.parse(resp.body)["items"]
+    #@username = JSON.parse(resp.body)["items"]["owner"]["login"]
   end
 
+
+
+  def create
+    resp = Faraday.post("https://api.github.com/uer/repos") do |req|
+      req.params['oauth_token'] = session[:token]
+      #req.params['v'] = '3'
+      req.params['name'] = params[:name]
+    end
+    redirect_to repositories_path
+  end
 end
+
