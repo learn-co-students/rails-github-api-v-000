db2a65e6d7f3f572fab640fcf4de3816b1b44b42
Deborah Seow <deborahseow9@gmail.com>
2018-08-24 14:21:19 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,10 +1,34 @@
 class RepositoriesController < ApplicationController
 
   def index
-    @resp = Faraday.get "https://api.github.com/user/repos" do |req|
-      req.params['Authorization'] = "token #{session[:token]}"
+    # @resp = Faraday.get "https://api.github.com/user/repos" do |req|
+    #   req.params['access_token'] = "#{session[:token]}"
+    # end
+    # @data = JSON.parse(@resp.body)
+    user = Faraday.get "https://api.github.com/user" do |req|
+      req.headers["Authorization"] = "token " + session[:token]
+      req.headers['Accept'] = 'application/json'
     end
-    @data = JSON.parse(@resp.body)
+
+    @user_data = JSON.parse(user.body)
+
+    repos = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.headers["Authorization"] = "token " + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @repo_data = JSON.parse(repos.body)
   end
 
+  def create
+    @resp = Faraday.post( "https://api.github.com/user/repos", {name:  params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'})
+    #   # req.body = {name: params[:name]}
+    #   # req.headers['Authorization'] = "token #{session[:token]}" req.headers['Accept'] = 'application/json'
+    #   req.params['name'] = params[:name]
+    #   req.params['access_token'] = "#{session[:token]}"
+    #   req.headers['Accept'] = 'application/json'
+    # end
+    @results = JSON.parse(@resp.body)
+    redirect_to root_path
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,30 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
-      # req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
-      # req.params['client_secret'] = ENV['GITHUB_SECRET']
-      # req.params['code'] = sessions[:code]
-      binding.pry
-      req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'], 'client_secret': ENV['GITHUB_SECRET'], 'code': session[:code] }
-      req.headers['Accept'] = 'application/json'
-    end
-    binding.pry
-    body = JSON.parse(resp.body)
-    session[:token] = body["access_token"]
-    redirect_to root_path
-  end
+
+   client_id = ENV['GITHUB_CLIENT_ID']
+   client_secret = ENV['GITHUB_CLIENT_SECRET']
+   code = params["code"]
+   resp = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+     req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+     req.headers['Accept'] = 'application/json'
+   end
+
+   body = JSON.parse(resp.body)
+   session[:token] = body['access_token']
+   redirect_to root_path
+ end
+  # def create
+  #   resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+  #     # req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+  #     # req.params['client_secret'] = ENV['GITHUB_SECRET']
+  #     # req.params['code'] = session[:code]
+  #     req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'], 'client_secret': ENV['GITHUB_SECRET'], 'code': params[:code] }
+  #     req.headers['Accept'] = 'application/json'
+  #   end
+  #   binding.pry
+  #   body = JSON.parse(resp.body)
+  #   session[:token] = body["access_token"]
+  #   redirect_to root_path
+  # end
 end
