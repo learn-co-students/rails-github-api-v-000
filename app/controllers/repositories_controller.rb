3dc15bb638d6cf88ee400383c1ecfd6d9b859c16
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-31 02:57:51 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,10 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.body['access_token'] = session[:token]
+    end
+    @repos = JSON.parse(resp.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user
+  skip_before_action :authenticate_user, only: :create
 
   def create
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
+    req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'] , 'client_secret': ENV['GITHUB_CLIENT_SECRET'], 'code': params[:code] }
+    req.headers['Accept'] = 'application/json'
   end
+  body = JSON.parse(response.body)
+  session[:token] = body['access_token']
+  redirect_to root_path
+end
+
 end
