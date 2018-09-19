6e4d76c5df5c4ee3f93dc919517b712cd0849f71
github username <github email address>
2018-09-18 19:22:58 -0500

    worked all day on this, followed exact instructions. Not passing

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,33 @@
 class RepositoriesController < ApplicationController
-  
+  skip_before_action :authenticate_user
+
   def index
+  	 user = Faraday.get "https://api.github.com/user" do |req|
+  	 	# binding.pry
+  	 req.headers['Authorization'] = 'token' + session[:token]
+
+  	 req.headers['Accept'] = 'application/json'
+  	 end
+
+  	 @user_data = JSON.parse(user.body)
+
+  	 repos = Faraday.get "https://api.github.com/user/repos" do |req|
+  	 req.params['Authorization'] = "token" + session[:token]
+  	 req.params['Accept'] = 'application/json'
+  	 end
+
+  	 @repo_data = JSON.parse(repos.body)
+  	end
+  	  	 
+
+  def create
+  	resp = Faraday.post "https://api.github.com/user/repos" do |req|
+  	req.body = { 'name': params[:name] }.to_json
+  	req.headers['Authorization'] = 'token' + session[:token] #see if it's refreshing
+  	req.headers['Accept'] = 'application/json'
+  end
+
+  	redirect_to '/'
   end
 
 end
