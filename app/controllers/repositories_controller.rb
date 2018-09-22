a8bb251b4ff451d1adfa77121ca1e878f88f7f85
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-22 19:14:18 -0400

    Retrieve and display username on repos index view

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,13 @@
 class RepositoriesController < ApplicationController
   
   def index
+    resp = Faraday.get('https://api.github.com/user') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+
+    body_json = JSON.parse(resp.body)
+
+    @username = body_json['login']
   end
 
 end
