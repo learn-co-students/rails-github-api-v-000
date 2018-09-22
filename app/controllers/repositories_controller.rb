bf7796821a46c3a733b969c372855ff20b0a2fe4
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-09-22 19:21:33 -0400

    Retrieve and list authenticated user's repos on repo index view

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,10 +4,13 @@ class RepositoriesController < ApplicationController
     resp = Faraday.get('https://api.github.com/user') do |req|
       req.headers['Authorization'] = "token #{session[:token]}"
     end
-
     body_json = JSON.parse(resp.body)
-
     @username = body_json['login']
+
+    resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    @repos = JSON.parse(resp.body)
   end
 
 end
