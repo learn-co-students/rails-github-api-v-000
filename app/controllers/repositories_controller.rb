dab8ea47a521f5fc602532044043d838820db741
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-26 14:41:58 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,18 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    url = "https://api.github.com/user?access_token=#{session[:token]}"
+    info = Faraday.get(url)
+    body = JSON.parse(info.body)
+    @username = body["login"]
+
+    reposURL = "https://api.github.com/users/#{@username}/repos"
+    repo_info = Faraday.get(reposURL) do |req|
+      req.params[:per_page] = 50
+    end
+    @repos = JSON.parse(repo_info.body)
   end
 
 end
+
+# Change WebMock.disable_net_connect!(allow_localhost: true) to WebMock.allow_net_connect! in spec_helper.rb on line 35
