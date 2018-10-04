d78de78b1dc1bdf5c76b7f349b3ee6445a635864
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-04 12:54:52 -0400

    fixed github url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     client_id = ENV['GITHUB_CLIENT_ID']
     client_secret = ENV['GITHUB_CLIENT_SECRET']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
-    github_url = "https://github.com/login/oauth/authorize#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
     redirect_to github_url unless logged_in?
   end
 
