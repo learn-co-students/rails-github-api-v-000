09a5b8fa449246be8793438749a094d9a1f77d0e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-04 13:41:05 -0400

    index page

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,13 +2,12 @@ class RepositoriesController < ApplicationController
 
   def index
     user = Faraday.get("https://api.github.com/user") do |req|
-      req.headers["Accept"] = 'application/json'
-      req.headers["Authorization"] = `token #{session[:token]}`
+      req.headers["Authorization"] = "token #{session[:token]}"
     end
-    @user_info = JSON.parse(user.body)
+    @body = JSON.parse(user.body)
+
     repositories = Faraday.get("https://api.github.com/user/repos") do |req|
-      req.headers["Accept"] = 'application/json'
-      req.headers["Authorization"] = `token #{session[:token]}`
+      req.headers["Authorization"] = "token #{session[:token]}"
     end
     @repos = JSON.parse(repositories.body)
   end
