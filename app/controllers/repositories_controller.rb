bed5e6ca1d1eb2d63ca63c81276b54f34f5c2a5f
cbshields <cbshields@outlook.com>
2018-10-12 16:15:14 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,19 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    user_resp = Faraday.get("https://api.github.com/user") do |req|
+      token = session[:token]
+      req.body = {'oauth_token': token}
+    end
+
+    @user = JSON.parse(user_resp.body)
+
+    repos_resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      token = session[:token]
+      req.body = {'oauth_token': token}
+    end
+
+    @repos = JSON.parse(repos_resp.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,15 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      client_id = ENV['GITHUB_CLIENT_ID']
+      client_secret = ENV['GITHUB_CLIENT_SECRET']
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.headers['Accept'] = 'application/json'
+    end
+    body = JSON.parse(response.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
-end
+
+end #ends SessionsController
