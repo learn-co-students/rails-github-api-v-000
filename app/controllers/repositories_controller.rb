83f5433900d0e39470f082b046606efb2c7ffc83
cbshields <cbshields@outlook.com>
2018-10-12 16:31:45 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,6 @@ class RepositoriesController < ApplicationController
       token = session[:token]
       req.body = {'oauth_token': token}
     end
-    end
 
     @user = JSON.parse(user_resp.body)
 
