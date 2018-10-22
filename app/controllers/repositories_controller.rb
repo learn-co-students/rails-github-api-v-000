1c7bedaac920a84ac573a1a1c46bb416f074ca28
Jul Jen <juliajen.prog@gmail.com>
2018-10-22 23:52:46 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,21 @@
 class RepositoriesController < ApplicationController
   
   def index
+  	resp = Faraday.get("https://api.github.com/user/repos") do |req|
+  		req.headers['Authorization'] = "token #{session[:token]}"
+  		req.headers['Accept'] = 'application/json'
+  	end
+
+  	@repos = JSON.parse(resp.body)
+  end
+
+  def create
+  	resp = Faraday.post("https://api.github.com/user/repos") do |req|
+  		req.body = { name: params[:name] }.to_json
+  		req.headers['Authorization'] = "token #{session[:token]}"
+  		req.headers['Accept'] = "application/json"
+  	end
+  	redirect_to root_path
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,21 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
+# parsing and storing the token as a value
   def create
+    client_id = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
+    code = params['code']
+
+    resp = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.headers['Accept'] = 'application/json'
+    end
+ # parses the response body into a Ruby hash and stores this hash as the body variable
+ # Whatever key value pairs were sent by GitHub will now be available, including body['access_token'].
+    body = JSON.parse(resp.body)
+    session[:token] = body['access_token']
+    redirect_to root_path
   end
+
 end
