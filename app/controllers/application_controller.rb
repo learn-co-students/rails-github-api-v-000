fe6fde30720867007ee813ced7968f1cf76a6292
sgibson47 <sgibson47@gmail.com>
2018-10-26 14:55:27 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,18 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # GET https://github.com/login/oauth/authorize
+  # Parameters:
+  # client_id  string
+  # redirect_uri  string
   def authenticate_user
+    client_id = ENV['GITHUB_CLIENT_ID']
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
+    redirect_to github_url unless logged_in?
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,20 @@
 class RepositoriesController < ApplicationController
   
+  # GET /user
   def index
+    resp = Faraday.get("https://github.com/user") do |req|
+      req.headers['Authorization'] = 'token ' + session[:token] 
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @user = JSON.parse(resp.body)
+
+    repos = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @repos = JSON.parse(repos.body)
   end
 
 end
