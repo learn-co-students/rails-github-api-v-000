196c588efde67d5da8a4f2e1275168346cd26e95
Adam Duff <j.adam.duff@gmail.com>
2018-11-08 19:25:02 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user
     client_id = ENV['GITHUB_CLIENT_ID']
-    redirect_uri = CGI.escape("http://165.227.81.218:37734/auth")
-    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
-    redirect_to github_url unless logged_in?
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo" unless logged_in?
   end
 
   def logged_in?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,16 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
-      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
-      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
-      req.params['redirect_uri'] = "http://165.227.81.218:37734/auth"
-      req.params['code'] = params[:code]
+    client_id = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
+    code = params['code']
+    resp = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.headers['Accept'] = 'application/json'
     end
-    binding.pry
-    session[:token] = resp["access_token"]
+
+    body = JSON.parse(resp.body)
+    session[:token] = body['access_token']
     redirect_to root_path
   end
 end
