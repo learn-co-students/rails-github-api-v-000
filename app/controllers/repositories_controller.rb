52e01787bfd67398826eb09bdc1b1c3659bc5c1f
zep1994 <snaxbags@protonmail.com>
2018-11-17 19:06:39 -0600

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,9 @@ class RepositoriesController < ApplicationController
         req.headers['Authorization'] = 'token ' + session[:token]
         req.headers['Accept'] = 'application/json'
       end
+
        @user_data = JSON.parse(user.body)
+       
        repos = Faraday.get 'https://api.github.com/user/repos' do |req|
         req.headers['Authorization'] = 'token ' + session[:token]
         req.headers['Accept'] = 'application/json'
