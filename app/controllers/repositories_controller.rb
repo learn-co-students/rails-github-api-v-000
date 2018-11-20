13ef0fa5a9c64266b1b52e7725acce4ca27e77f9
github username <github email address>
2018-11-20 15:38:14 -0600

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,15 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos = JSON.parse(response.body)
   end
 
+  def create
+    resp = Faraday.post("https://github.com/user/repos") do |req|
+      req.params["name"] = params["name"].to_json
+      req.params['access_token'] = session[:token]
+    end
+    redirect_to '/'
+  end
 end
