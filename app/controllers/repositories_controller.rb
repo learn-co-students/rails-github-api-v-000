102787ecc903bf31e8177feaaa33c20b4495aadc
github username <github email address>
2019-01-05 15:15:11 -0500

    in process

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,24 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    user = Faraday.get ('https://api.github.com/user') do |req|
+      req.params['access_token'] = session[:token]
+    end
+      @repo_owner = JSON.parse(user.body)
+
+    repos = Faraday.get  ('https://api.github.com/user/repos') do |req|
+      req.params['access_token'] = session[:token]
+    end
+      @repo_data = JSON.parse(repos.body)
+
   end
 
-end
+  end
+
+  # def create
+  #   # resp = Faraday.post'https://api.github.com/user/repos' do |req|
+  #   #   req.body
+  #
+  # end
+
+# end
