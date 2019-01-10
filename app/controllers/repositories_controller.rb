99f8e526cd9d1bcffe27cb4110d0bdfd63518b95
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-01-10 15:35:10 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,9 +2,12 @@ class RepositoriesController < ApplicationController
 
   def create
     response = Faraday.post "https://api.github.com/user/repos" do |req|
-      req.body = {"name": "#{params[:name]}", "scope": "repo"}
-      # req.params["scope"] = "repo"
-      req.headers = {"Authorization": "token #{session[:token]}"}
+      # req.body = {"name": "#{params[:name]}", "scope": "repo"}
+      # # req.params["scope"] = "repo"
+      # req.headers = {"Authorization": "token #{session[:token]}"}
+      req.body = { 'name': params[:name] }.to_json
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
     end
     redirect_to root_path
     # binding.pry
