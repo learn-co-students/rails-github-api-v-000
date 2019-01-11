3db8c02c70c89f5599f9985414aba2d9d255846f
JerYoMat <jeremy.emata@gmail.com>
2019-01-11 18:22:14 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,30 @@
 class RepositoriesController < ApplicationController
-  
+ #3. Use the access token to access the API
+ #GET https://api.github.com/user?access_token=...
+ #create instance variable to pass data on to view 
   def index
+    user = Faraday.get 'https://api.github.com/user' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @user_data = JSON.parse(user.body)
+
+    repos = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @repo_data = JSON.parse(repos.body)
   end
 
+  #Create a new repository on github
+  def create
+    response = Faraday.post 'https://api.github.com/user/repos' do |req|
+      req.body = { 'name': params[:name] }.to_json
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+    redirect_to '/'
+  end
 end
