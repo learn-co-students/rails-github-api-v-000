256a26c5945c855a53601f39dd9b61b0c4af674d
FKRauber <fabienne.rauber@gmail.com>
2019-02-07 18:29:01 -0500

    added repositories_controller create method

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -15,4 +15,13 @@ class RepositoriesController < ApplicationController
 
   end
 
+  def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.body = { 'name': params[:name] }.to_json
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+    redirect_to '/'
+  end
+
 end
