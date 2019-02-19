4203157283da0c0597a5573e4f2f8839036e060a
epic0448 <samuelogundiran@Yahoo.com>
2019-02-19 11:33:55 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,4 +3,34 @@ class RepositoriesController < ApplicationController
   def index
   end
 
+  def create
+
+    client_id = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
+
+    @resp = Faraday.post 'https://api.github.com/user/repos' do |req|
+      binding.pry
+
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    render 'search'
+
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+      render 'search'
+  end
+
+
 end
+
+
+#  not sure.. where to go from here.
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,23 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
-  def create
-  end
+  
+
+    # skip_before_action :authenticate_user, only: :create
+    
+    def create
+        response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+          
+          req.body = {'GITHUB_CLIENT_ID': ENV["GITHUB_CLIENT_ID"], 'GITHUB_CLIENT_SECRET': ENV["GITHUB_CLIENT_SECRET"]}
+            req.headers['Accept'] = 'application/json'
+          
+        end
+       
+        body = JSON.parse(respone.body)
+        session[:token] = body["access_token"]
+        redirect_to root_path
+      end
+
+
+
 end
