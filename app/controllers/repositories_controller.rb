2d08fcdecebac7c60a27d927905ebb6642517c77
amitnyc83 <amit_nyc@live.com>
2019-02-20 15:17:22 -0500

    adds oauth

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,21 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    resp = Faraday.get("https://api.github.com/self/repos") do |req|
+      req.params['oauth_token'] = session[:token]
+      req.params['name'] = params[:name]
+    end
+    @repos = JSON.parse(resp.body)
   end
+
+  def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.params["oauth_token"] = session[:token]
+      req.params['name'] = params[:name]
+    end
+    redirect_to '/'
+  end
+
+
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,19 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret']= ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params[:code]
+      req.headers['Accept'] = 'application/json'
+    end
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
+
+
+
+
+# POST https://github.com/login/oauth/access_token
