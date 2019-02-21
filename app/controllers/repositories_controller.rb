fd92dc042de336d543a1fb95d27c51b67a4fb526
amitnyc83 <amit_nyc@live.com>
2019-02-21 14:06:22 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,21 +1,30 @@
+require 'pry'
+
 class RepositoriesController < ApplicationController
 
   def index
-    resp = Faraday.get("https://api.github.com/self/repos") do |req|
-      req.params['oauth_token'] = session[:token]
-      req.params['name'] = params[:name]
+    user = Faraday.get("https://api.github.com/user") do |req|
+      #req.params['oauth_token'] = session[:token]
+      req.headers["Authorization"] = 'token ' + session[:token]
+      req.headers["Accept"] = 'application/json'
+    end
+    @user = JSON.parse(user.body)
+
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers["Authorization"] = 'token ' + session[:token]
+      req.headers["Accept"] = 'application/json'
     end
     @repos = JSON.parse(resp.body)
   end
 
   def create
+    name = params[:name]
     resp = Faraday.post("https://api.github.com/user/repos") do |req|
-      req.params["oauth_token"] = session[:token]
-      req.params['name'] = params[:name]
+      req.body = {"name" => name}.to_json
+      # req.params['name'] = params[:name]
+      req.headers["Authorization"] = 'token '  + session[:token]
+      req.headers["Accept"] = 'application/json'
     end
     redirect_to '/'
   end
-
-
-
 end
