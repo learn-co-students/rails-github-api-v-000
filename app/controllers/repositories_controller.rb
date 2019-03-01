684c8bba5001072d51ecaf6a205354c56807b40b
github username <github email address>
2019-03-01 14:07:55 -0500

    Display authorized user name and repositories

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,23 @@
 class RepositoriesController < ApplicationController
   
   def index
+    begin
+
+        user_resp = Faraday.get 'https://api.github.com/user' do |req|
+            req.headers['Authorization'] = "token " + session[:token]
+            req.headers['Accept'] = 'application/vnd.github.v3+json'
+        end
+        @user = JSON.parse(user_resp.body)
+
+        repos_resp = Faraday.get 'https://api.github.com/user/repos' do |req|
+            req.headers['Authorization'] = 'token ' + session[:token]
+            req.headers['Accept'] = 'application/vnd.github.v3+json'
+          end
+        @repos = JSON.parse(repos_resp.body)
+
+    rescue Faraday::TimeoutError, Faraday::ConnectionFailed
+      @error = "There was a network problem. Please try again."
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/auth' => 'sessions#create'
-  post '/repositories/create' => 'repositories#create'
-  root 'repositories#index'
-
+    root 'repositories#index'    
+    get '/auth' => 'sessions#create'
+    post '/repositories/create' => 'repositories#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
