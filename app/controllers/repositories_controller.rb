635afc6dd8765ab4cda5c6fc0d721bc5dae5f94b
github username <github email address>
2019-03-02 12:42:37 -0800

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,4 +1,5 @@
 class RepositoriesController < ApplicationController
+  require "pry"
   
   def index
     user = Faraday.get("https://api.github.com/user") do |req|
@@ -12,6 +13,16 @@ class RepositoriesController < ApplicationController
       req.headers['Accept'] = 'application/json'
     end
     @repo_data = JSON.parse(repos.body)
+  end
+
+  def create
+    name = params[:name]
+    new_repo = Faraday.post('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+      req.body = {'name': name, 'description': "creating repos with APIs and OAuth."}.to_json
+    end
+    redirect_to '/'
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
       client_id = ENV['GITHUB_CLIENT_ID']
       client_secret = ENV['GITHUB_CLIENT_SECRET']
       code = params['code']
-      resp = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
-        req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      resp = Faraday.post 'https://github.com/login/oauth/access_token?scope=user:repo' do |req|
+        req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code}
         req.headers['Accept'] = 'application/json'
       end
   
