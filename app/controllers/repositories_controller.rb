710597d3516d27e4e058483887b6abddac43698d
Bruce Hillsworth <brucehillsworth@Bruces-iMac.local>
2019-03-11 18:17:54 -0600

    First Commit

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,15 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    @resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.params['client_id'] = ENV['CLIENT_ID']
+      req.params['client_secret'] = ENV['CLIENT_SECRET']
+      req.params['access_token'] = session[:token]
+    end
+    @body_hash = JSON.parse(@resp.body)
+    render 'index'
   end
 
+  def create
+  end
 end
