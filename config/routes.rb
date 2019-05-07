b4ae8e0646dac25d3ed83c18a18fb1d9a70fc7c9
George  Pianka <gpianka1@gmail.com>
2019-05-07 05:07:07 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'repositories#index'
   get '/auth' => 'sessions#create'
-    post '/repositories/create' => 'repositories#create'
-    root 'repositories#index'
+  post '/repositories/create' => 'repositories#create'
+    
 end
