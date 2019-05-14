3330f6c83c7e232cd8941e7ed21fe9864b6915d5
github username <github email address>
2019-05-14 18:18:35 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,13 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    @resp = Faraday.get "https://api.github.com/user" do |req|
+    # req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'], 'client_secret': ENV['GITHUB_CLIENT_SECRET'], 'code': params[:code] }
+    req.headers['Authorization'] = "token #{session[:token]}"
+    
+    end
   end
 
+  def create
+  end
 end
