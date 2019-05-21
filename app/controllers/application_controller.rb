1d3911991d572d813679089b5eebc83a9b861321
Rachel Wyatt <rw126@evansville.edu>
2019-05-21 07:33:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,10 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
