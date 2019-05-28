26d1d53f048d9c529d7f6cfec33cd1021bd1ed88
hejeong <jonhong777@gmail.com>
2019-05-28 12:12:04 -0400

    Add broken code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user
+  skip_before_action :authenticate_user, only: create
 
   def create
+    response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'], 'client_secret': ENV['GITHUB_CLIENT_ID'], 'code': params[:code] }
+      req.headers['Accept'] = 'application/json'
+    end
+    body = JSON.parse(response.body)
+    session[:token] = body['access_token']
+    user_response = Faraday.get 'https://api.github.com/user', {}, 'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json['login']
+    redirect_to '/'
   end
 end
