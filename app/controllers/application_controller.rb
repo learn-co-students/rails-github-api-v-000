da2e06811da183d773b3a35c1fce029df15a34cc
corytveten <corytveten@gmail.com>
2019-06-08 07:02:48 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    client_id = ENV[GITHUB_CLIENT_ID]
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
