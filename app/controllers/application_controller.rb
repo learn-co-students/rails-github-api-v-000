eccc1df6ca47bebbedd37e78ae38ec803de52e7d
Grace Choi <oiograceoio@gmail.com>
2019-06-11 21:46:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user
+  require 'pry'
 
   private
 
   def authenticate_user
+  	client_id = ENV['GITHUB_CLIENT_ID']
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
+    redirect_to github_url unless logged_in?
   end
 
   def logged_in?
+  	!!session[:token]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user
+  skip_before_action :authenticate_user, only: :create
 
-  def create
+def create
+    resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT_ID"], client_secret: ENV["GITHUB_CLIENT_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user = JSON.parse(user_response.body)
+    session[:username] = user["login"]
+    binding.pry
+    redirect_to root_path
   end
 end
+
