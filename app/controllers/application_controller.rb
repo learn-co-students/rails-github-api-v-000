52ea7206a77c528b1edd82f9365f9c014b07e8cd
inkanimal <theatomproject@gmail.com>
2019-06-15 12:58:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,16 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    # redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT_ID']}&scope=repo" if !logged_in?
+    client_id = ENV['GITHUB_CLIENT_ID']
+      redirect_uri = CGI.escape('http://localhost:3000/auth')
+      scope = 'repo'
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=#{scope}"
+
+      redirect_to github_url unless logged_in?  
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,30 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+
+    response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      req.body = { client_id: ENV["GITHUB_CLIENT_ID"], client_secret: ENV["GITHUB_SECRET"], code: params[:code] }
+      req.headers['Accept'] = 'application/json'
+    end
+    session[:token] = JSON.parse(response.body)['access_token']
+
+  # resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
+  #   req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+  #   req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+  #   req.params['redirect_uri'] = 'http://localhost:3000/auth'
+  #   req.params['code'] = params[:code]
+    
+  #   req.headers = {'Accept': 'application/json'}
+  # end
+  # session[:token] = JSON.parse(resp.body)['access_token']
+  
+  
+  user_data = Faraday.get('https://api.github.com/user') do |req|
+    req.params['access_token'] = session[:token]
+    req.headers = {'Accept': 'application/json'}
   end
+  session[:username] = JSON.parse(user_data.body)['login']
+
+  redirect_to root_path
+end
 end
