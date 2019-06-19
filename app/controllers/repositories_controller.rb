168f93f35e2d999b79cd99b022761bdbe994fca0
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-18 23:12:44 -0400

    tests pass

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,8 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    response = Faraday.get 'https://api.github.com/user/repos', {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    @repos = JSON.parse(response.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,20 @@ class SessionsController < ApplicationController
   def create
     response = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
       req.body = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'code': code
-      }
+        client_id: ENV['GITHUB_CLIENT_ID'],
+        client_secret: ENV['GITHUB_CLIENT_SECRET'],
+        code: params[:code]}
       req.headers['Accept'] = 'application/json'
     end
-    body = JSON.parse(respomse.body)
+
+    body = JSON.parse(response.body)
     session[:token] = body['access_token']
+
+    user_info = Faraday.get 'https://api.github.com/user', {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+
+    user_json = JSON.parse(user_info.body)
+    session[:username] = user_json['login']
+
     redirect_to root_path
   end
 end
