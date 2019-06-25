62355107ffac79a633d63633634666b10ea8b939
valeria campilongo <cvaleriac@hotmail.it>
2019-06-25 18:56:32 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,12 +12,13 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.before(:each) do
+
     stub_request(:get, "https://api.github.com/user/repos").
       with(:headers => {'Authorization'=>'token 1'}).
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:post, "https://github.com/login/oauth/access_token").
-      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20"},
+      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_SECRET"], "code"=>"20"},
       :headers => {'Accept'=>'application/json'}).
       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
 
@@ -29,6 +30,7 @@ RSpec.configure do |config|
       with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
       :headers => {'Authorization'=>'token 1'}).
       to_return(:status => 201, :body => "", :headers => {})
+
   end
 end
 
