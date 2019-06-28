2a2f719ace24ad2da941b47aadbdf46157945cf2
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-28 22:57:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user
 
   private
-
   def authenticate_user
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
