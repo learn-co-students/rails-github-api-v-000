ed47e477efcb894cece9314c831c41ab09b71485
github username <github email address>
2019-07-03 13:25:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth' => 'sessions#create'
-  get "/login/oauth/authorize" => 'repositories#index'
   post '/repositories/create' => 'repositories#create'
   root 'repositories#index'
 end
