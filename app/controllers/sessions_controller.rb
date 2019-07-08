cc3776b65bd12c5748b6e1186c0e6c06f29c0beb
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-07-07 19:20:07 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,13 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      # binding.pry
+      req.body = { 'client_id': ENV['GITHUB_CLIENT'], 'client_secret': ENV['GITHUB_SECRET'], 'code': params[:code] }
+      req.headers['Accept'] = 'application/json'
+    end
+    body = JSON.parse(response.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
