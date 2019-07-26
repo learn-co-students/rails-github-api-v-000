18217a9f3bf0fed8ddcdac25324bcf59c505ba12
sjwakean <swakeman@ca.rr.com>
2019-07-26 14:58:44 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,9 @@
 class RepositoriesController < ApplicationController
-  
+  #Call the GitHub API from within repositories#index 
+  #to retrieve and display the current user's 'login' 
+  #in repositories/index.html.erb.
   def index
-    
+    "https://api.github.com/login/username"
   end
 
 end
