7baa7e675e20e6d6387b9f2e7aec88e5bb86ba09
sjwakean <swakeman@ca.rr.com>
2019-07-26 20:24:35 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,11 @@ class RepositoriesController < ApplicationController
   #to retrieve and display the current user's 'login' 
   #in repositories/index.html.erb.
   def index
-    "https://api.github.com/login/username"
+    response = Faraday.get "https://api.github.com/user/repos" do  |req|
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+      @repos = JSON.parse(response.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,16 @@ class SessionsController < ApplicationController
     end
     body = JSON.parse(response.body)
       session[:token] = body['access_token']
-      redirect_to '/'
+      
+
+    user_response = Faraday.get "https://api.github.com/user" do  |req|
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+  
+    user = JSON.parse(user_response.body)
+      session[:username] = user['login']
+
+    redirect_to '/'
   end
 end
