e92c8fb77436c2d3b2d7b4fbc25fb49d43907853
Jonathan Luk <jluk87@gmail.com>
2019-08-29 01:19:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    client_id = ENV['GITHUB_CLIENT']
+    redirect_uri = 'http://67.205.182.198:53917/auth'
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}" unless logged_in?
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
