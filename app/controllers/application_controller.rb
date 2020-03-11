db284efec9684b0b9855597bbd8a84b8e67d07d7
Kat Tochowicz <ktochow1@binghamton.edu>
2020-03-11 16:48:11 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :authenticate_user
 
   private
 
-  def authenticate_user
+  def logged_in?
+    !!session[:token]
   end
 
-  def logged_in?
+  def authenticate_user
+     # github_client = ENV['GITHUB_CLIENT']
+     redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+      # redirect_to "https://github.com/login/oauth/authorize" unless logged_in?
   end
 end
